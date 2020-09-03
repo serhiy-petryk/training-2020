4c65f5a54cc19a4d8dd17aae0dad0383d585ab63
serhiy-petryk <sergei_petrik@yahoo.com>
2020-09-03 03:40:29 +0300

    refactored SplitButton

---
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/SplitButton.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WpfInvestigate.Controls
+{
+    /// <summary>
+    /// Interaction logic for SplitButton.xaml
+    /// </summary>
+    public partial class SplitButton : ResourceDictionary
+    {
+        public SplitButton()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenDropDownMenu(object sender, RoutedEventArgs e) => DropDownButton.OpenDropDownMenu(sender);
+    }
+}
