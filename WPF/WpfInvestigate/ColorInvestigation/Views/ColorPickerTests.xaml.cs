445f1efb4991330944dad7fc9649cb13bf51a556
serhiy-petryk <sergei_petrik@yahoo.com>
2020-09-18 18:30:39 +0300

    Before remove ViewModel in ColorPicker

--- WPF/WpfInvestigate/ColorInvestigation/Views/ColorPickerTests.xaml.cs
+++ WPF/WpfInvestigate/ColorInvestigation/Views/ColorPickerTests.xaml.cs
@@ -17,14 +17,10 @@ namespace ColorInvestigation.Views
         private void SaveButton_Click(object sender, RoutedEventArgs e) => ColorPicker.SaveColor();
         private void SaveButtonVM_Click(object sender, RoutedEventArgs e) => ColorPickerViewModel.SaveColor();
         private void SaveButtonAsync_Click(object sender, RoutedEventArgs e) => ColorPickerAsync.SaveColor();
-        private void SaveButtonDelay_Click(object sender, RoutedEventArgs e) => ColorPickerDelay.SaveColor();
         private void SaveButtonLabel_Click(object sender, RoutedEventArgs e) => ColorPickerLabel.SaveColor();
 
         private void RestoreButton_Click(object sender, RoutedEventArgs e) => ColorPicker.RestoreColor();
         private void RestoreButtonVM_Click(object sender, RoutedEventArgs e) => ColorPickerViewModel.RestoreColor();
-        private void RestoreButtonAsync_Click(object sender, RoutedEventArgs e) => ColorPickerAsync.RestoreColor();
-        private void RestoreButtonDelay_Click(object sender, RoutedEventArgs e) => ColorPickerDelay.RestoreColor();
-        private void RestoreButtonLabel_Click(object sender, RoutedEventArgs e) => ColorPickerLabel.RestoreColor();
 
         private void TestButton_Click(object sender, RoutedEventArgs e)
         {
@@ -37,6 +33,14 @@ namespace ColorInvestigation.Views
                 BrushVM.Color = Colors.Yellow;
             else
                 BrushVM.Color = Colors.Blue;
+        }
+
+        private void ChangeColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Brush.Color == Colors.Blue)
+                Brush.Color = Colors.Yellow;
+            else
+                Brush.Color = Colors.Blue;
         }
     }
 }
