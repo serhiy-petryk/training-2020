192896945f44b79200aa5f0cfdb615d1b9e4736e
serhiy-petryk <sergei_petrik@yahoo.com>
2020-09-23 11:11:04 +0300

    Update MainWindow.xaml.cs

--- WPF/WpfInvestigate/ColorInvestigation/MainWindow.xaml.cs
+++ WPF/WpfInvestigate/ColorInvestigation/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Media;
 using ColorInvestigation.Common;
 using ColorInvestigation.Lib;
 using ColorInvestigation.Views;
@@ -19,25 +18,15 @@ namespace ColorInvestigation
 
         private void OnTestButtonClick(object sender, RoutedEventArgs e)
         {
-            var a32 = new ColorSpaces.XYZ(new ColorSpaces.RGB(Color.FromRgb(0, 0, 0x9E)));
-            var a42 = new ColorSpaces.LAB(new ColorSpaces.RGB(Color.FromRgb(0, 0, 0x20)));
-            var rgb = new ColorSpaces.RGB(Color.FromRgb(0, 0, 0x20));
-            var lab = new ColorSpaces.LAB(rgb);
             // ColorSpacesTests.HueTest();
             // ColorSpacesTests.HslTest();
             // ColorSpacesTests.HsvTest();
-            ColorSpacesTests.XyzTest();
+            // ColorSpacesTests.LabTest();
+            // ColorSpacesTests.XyzTest();
 
-            var c21 = Color.FromRgb(169, 104, 54);
-            var c22 = new ColorSpaces.YCbCr(new ColorSpaces.RGB(c21), ColorSpaces.YCbCrStandard.BT601);
-            var c23 = c22.RGB.Color;
-            ColorSpacesTests.YCbCrTest(ColorSpaces.YCbCrStandard.BT601);
+            // ColorSpacesTests.YCbCrTest(ColorSpaces.YCbCrStandard.BT601);
             ColorSpacesTests.YCbCrTest(ColorSpaces.YCbCrStandard.BT709);
             ColorSpacesTests.YCbCrTest(ColorSpaces.YCbCrStandard.BT2020);
-
-
-            // ColorXyz.Test(); // OK! 
-            // ColorLab.Test(); // OK! 
         }
 
         private bool IsEqual(double d1, double d2) => Math.Abs(d1 - d2) < 0.0001;
