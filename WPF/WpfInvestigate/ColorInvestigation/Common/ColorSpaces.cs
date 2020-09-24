cf30089123aee687b84d89d405d0aa0a9c2ba310
serhiy-petryk <sergei_petrik@yahoo.com>
2020-09-25 01:34:48 +0300

    Update ColorSpaces.cs

--- WPF/WpfInvestigate/ColorInvestigation/Common/ColorSpaces.cs
+++ WPF/WpfInvestigate/ColorInvestigation/Common/ColorSpaces.cs
@@ -30,7 +30,36 @@ namespace ColorInvestigation.Common.ColorSpaces
     {
         static ColorUtils()
         {
-            var type = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.Namespace == "System.Windows.Media" && t.Name == "KnownColor");
+            // From https://www.w3.org/TR/css-color-3/
+            var colorNames = new string[]
+            {
+                "aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black",
+                "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate",
+                "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod",
+                "darkgray", "darkgreen", /*"darkgrey",*/ "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange",
+                "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray",
+                /*"darkslategrey",*/ "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", /*"dimgrey",*/
+                "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold",
+                "goldenrod", "gray", "green", "greenyellow", /*"grey",*/ "honeydew", "hotpink", "indianred", "indigo",
+                "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral",
+                "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", /*"lightgrey",*/ "lightpink", "lightsalmon",
+                "lightseagreen", "lightskyblue", "lightslategray", /*"lightslategrey",*/ "lightsteelblue", "lightyellow",
+                "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid",
+                "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise",
+                "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy",
+                "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen",
+                "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue",
+                "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen",
+                "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", /*"slategrey",*/ "snow", "springgreen",
+                "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke",
+                "yellow", "yellowgreen"
+            };
+            foreach (var colorName in colorNames)
+                if (!KnownColors.ContainsKey(colorName))
+                    KnownColors.Add(colorName, StringToColor(colorName));
+
+
+            /*var type = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.Namespace == "System.Windows.Media" && t.Name == "KnownColor");
             if (type != null)
             {
                 var nameList = Enum.GetValues(type).OfType<object>().Skip(1).Select(a => a.ToString()).OrderBy(a => a).ToList();
@@ -38,7 +67,7 @@ namespace ColorInvestigation.Common.ColorSpaces
                 foreach (var colorName in nameList)
                     if (!KnownColors.ContainsKey(colorName))
                         KnownColors.Add(colorName, StringToColor(colorName));
-            }
+            }*/
         }
 
         /// <summary>
