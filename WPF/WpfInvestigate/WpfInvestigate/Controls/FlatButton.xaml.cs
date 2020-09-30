a10af061f8b5dce666a27ad1a37f76e7a121fd12
serhiy-petryk <sergei_petrik@yahoo.com>
2020-09-30 13:28:06 +0300

    x

--- WPF/WpfInvestigate/WpfInvestigate/Controls/FlatButton.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/FlatButton.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.ComponentModel;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
-using WpfInvestigate.Common;
-
-namespace WpfInvestigate.Controls
+﻿namespace WpfInvestigate.Controls
 {
     /// <summary>
     /// Interaction logic for FlatButton.xaml
