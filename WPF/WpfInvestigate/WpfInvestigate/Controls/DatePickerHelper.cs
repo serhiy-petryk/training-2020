29e337ec5c0418d01eb73ec68f877005a56ed4dc
serhiy-petryk <sergei_petrik@yahoo.com>
2020-10-05 03:46:07 +0300

    fixed border and background of DatePickerTextBox

---
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/DatePickerHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using WpfInvestigate.Common;
+
+namespace WpfInvestigate.Controls
+{
+    public static class DatePickerHelper
+    {
+        public static void HideBorderOfTextBox(FrameworkElement picker)
+        {
+            var borderNames = new[] { "watermark_decorator", "ContentElement" };
+            var textBox = picker is DatePickerTextBox
+                ? picker as DatePickerTextBox
+                : Tips.GetVisualChildren(picker).OfType<DatePickerTextBox>().FirstOrDefault();
+
+            //  Remove textbox background
+            if (textBox != null && textBox.Background != Brushes.Transparent)
+                textBox.Background = Brushes.Transparent;
+
+            // Hide border of DatePicker textbox
+            var borders = Tips.GetVisualChildren(picker).OfType<Border>().Where(c => borderNames.Contains(c.Name));
+            foreach (var x in borders)
+                x.BorderBrush = Brushes.Transparent;
+
+        }
+    }
+}
---
+++ WPF/WpfInvestigate/WpfInvestigate/Theme/Common.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WpfInvestigate.Theme
+{
+    public partial class Common
+    {
+        public Common()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDatePickerTextBoxLoaded(object sender, RoutedEventArgs e)
+        {
+            Controls.DatePickerHelper.HideBorderOfTextBox((FrameworkElement) sender);
+        }
+    }
+}
