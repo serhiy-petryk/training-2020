003ac4e17775ff22eaee8f781de78069422de6bc
serhiy-petryk <sergei_petrik@yahoo.com>
2020-10-11 10:41:18 +0300

    added chrome tests

--- WPF/WpfInvestigate/WpfInvestigate/MainWindow.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/MainWindow.xaml.cs
@@ -61,6 +61,7 @@ namespace WpfInvestigate
         private void ColorPickerTest_OnClick(object sender, RoutedEventArgs e) => new ColorPickerTests().Show();
         private void ControlEffectTests_OnClick(object sender, RoutedEventArgs e) => new ControlEffectTests().Show();
         private void BootstrapButtonTests_OnClick(object sender, RoutedEventArgs e) => new BootstrapButtonTests().Show();
+        private void ChromeTest_OnClick(object sender, RoutedEventArgs e) => new ChromeTests().Show();
 
         private void OnTestButtonClick(object sender, RoutedEventArgs e)
         {
---
+++ WPF/WpfInvestigate/WpfInvestigate/TestViews/ChromeTests.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace WpfInvestigate.TestViews
+{
+    /// <summary>
+    /// Interaction logic for ChromeTests.xaml
+    /// </summary>
+    public partial class ChromeTests : Window
+    {
+        public ChromeTests()
+        {
+            InitializeComponent();
+        }
+
+        private void TestButton_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Element_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            var fe = (FrameworkElement)sender;
+            Debug.Print($"GotFocus: {fe.Name}");
+        }
+
+        private void Element_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            var fe = (FrameworkElement)sender;
+            Debug.Print($"LostFocus: {fe.Name}");
+        }
+    }
+}
