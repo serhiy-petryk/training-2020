82c24bd00d00b937d01bc4a269a399b2f86910dd
serhiy-petryk <sergei_petrik@yahoo.com>
2020-10-12 13:40:36 +0300

    refactored pathdata effect

--- WPF/WpfInvestigate/WpfInvestigate/Controls/Effects/PathDataEffect.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/Effects/PathDataEffect.cs
@@ -1,8 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfInvestigate.Controls.Effects
 {
@@ -12,57 +14,48 @@ namespace WpfInvestigate.Controls.Effects
     {
         public static readonly DependencyProperty PathDataProperty = DependencyProperty.RegisterAttached(
             "PathData", typeof(Geometry), typeof(PathDataEffect), new UIPropertyMetadata(null, OnPathDataChanged));
+
         public static Geometry GetPathData(DependencyObject obj) => (Geometry)obj.GetValue(PathDataProperty);
         public static void SetPathData(DependencyObject obj, Geometry value) => obj.SetValue(PathDataProperty, value);
 
         private static void OnPathDataChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d is ContentControl control)
-            {
-                if (e.NewValue is Geometry geometry)
-                {
-                    var path = new Path { Stretch = Stretch.Uniform, Data = geometry };
-                    var viewbox = new Viewbox {Child = path};
-                    if (control.HasContent)
-                    {
-                        var grid = new Grid {VerticalAlignment = VerticalAlignment.Stretch, Background = Brushes.Red};
-                        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
-                        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-                        grid.Children.Add(viewbox);
-                        Grid.SetColumn(viewbox, 0);
-                        var oldContent = control.Content;
-                        if (oldContent is string)
-                        {
-                            var textBlock = new TextBlock
-                            {
-                                Text = (string) oldContent, Foreground = Brushes.Black,
-                                Margin = new Thickness(4, 0, 0, 0),
-                                VerticalAlignment = VerticalAlignment.Center
-                            };
-                            var b = new Binding("Foreground")
-                            {
-                                RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(Control), 1)
-                            };
-                            textBlock.SetBinding(Control.ForegroundProperty, b);
-                            oldContent = textBlock;
-                        }
+            if (!(d is ContentControl control) || !(e.NewValue is Geometry geometry)) return;
 
-                        control.Content = null;
-                        grid.Children.Add(oldContent as UIElement);
-                        Grid.SetColumn(oldContent as UIElement, 1);
-                        control.Content = grid;
-                    }
-                    else
-                        control.Content = viewbox;
-                    /*var b = new Binding("Foreground")
+            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() =>
+            {
+                var path = new Path { Stretch = Stretch.Uniform, Data = geometry };
+                var viewbox = new Viewbox { Child = path };
+                if (control.Content is string || control.Content is UIElement)
+                {
+                    viewbox.Margin = new Thickness(0, 0, 4, 0);
+                    var panel = new StackPanel
                     {
-                        RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(Control), 1)
+                        Orientation = Orientation.Horizontal,
+                        VerticalAlignment = VerticalAlignment.Stretch
                     };
-                    path.SetBinding(Shape.FillProperty, b);*/
+                    panel.Children.Add(viewbox);
+
+                    var content = control.Content;
+                    if (content is string)
+                    {
+                        var textBlock = new TextBlock{ Text = (string)content, VerticalAlignment = VerticalAlignment.Center };
+                        var b = new Binding("Foreground")
+                        {
+                            RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(Control), 1)
+                        };
+                        textBlock.SetBinding(Control.ForegroundProperty, b);
+                        content = textBlock;
+                    }
+
+                    control.Content = null;
+                    panel.Children.Add(content as UIElement);
+                    control.Content = panel;
                 }
                 else
-                    control.Content = null;
-            }
+                    control.Content = viewbox;
+            }));
         }
     }
 }
+
