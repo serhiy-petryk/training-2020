2a6fd070f0424bd722169e3dcfb88f5752262de2
serhiy-petryk <sergei_petrik@yahoo.com>
2020-10-27 13:50:46 +0200

    added ControlInvestigation project

---
+++ WPF/WpfInvestigate/ControlInvestigate/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using ControlInvestigate.Tests;
+
+namespace ControlInvestigate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TempControl_OnClick(object sender, RoutedEventArgs e) => new TempControl().Show();
+    }
+}
---
+++ WPF/WpfInvestigate/ControlInvestigate/Tests/TempControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ControlInvestigate.Tests
+{
+    /// <summary>
+    /// Interaction logic for TempControl.xaml
+    /// </summary>
+    public partial class TempControl : Window
+    {
+        public TempControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
