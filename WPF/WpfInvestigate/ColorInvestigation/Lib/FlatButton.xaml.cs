f7ab97ed062216775a7691aaca46653a75e9ce43
serhiy-petryk <sergei_petrik@yahoo.com>
2020-10-30 01:16:39 +0200

    added test object in ControlInvestigate

--- WPF/WpfInvestigate/ColorInvestigation/Lib/FlatButton.xaml.cs
+++ WPF/WpfInvestigate/ColorInvestigation/Lib/FlatButton.xaml.cs
@@ -5,9 +5,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
 using System.Windows.Media;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using ColorInvestigation.Common;
 
---
+++ WPF/WpfInvestigate/ControlInvestigate/Temp/TestObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlInvestigate.Temp
+{
+    public class TestObject: Control
+    {
+        static TestObject()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TestObject), new FrameworkPropertyMetadata(typeof(TestObject)));
+        }
+
+        public string TestProperty => "Test";
+
+        public TestObject()
+        {
+            DataContext = this;
+        }
+
+    }
+}
