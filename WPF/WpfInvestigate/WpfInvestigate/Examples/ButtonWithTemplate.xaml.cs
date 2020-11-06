bc9974589204561937e7b0824341c27aae35cb0d
serhiy-petryk <sergei_petrik@yahoo.com>
2020-11-06 02:57:20 +0200

    added control examples in WpfInvestigate

---
+++ WPF/WpfInvestigate/WpfInvestigate/Examples/ButtonWithTemplate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfInvestigate.Examples
+{
+    /// <summary>
+    /// Interaction logic for ControlExampleWithTemplate.xaml
+    /// </summary>
+    public partial class ButtonWithTemplate : Button
+    {
+        public ButtonWithTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
