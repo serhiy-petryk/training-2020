81884eedef231cf52ecf76bbf832d601d3aa4a4a
serhiy-petryk <sergei_petrik@yahoo.com>
2020-11-07 17:32:47 +0200

    added UserControlExample

---
+++ WPF/WpfInvestigate/WpfInvestigate/Examples/UserControlExample.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfInvestigate.Examples
+{
+    /// <summary>
+    /// Interaction logic for ControlExample.xaml
+    /// </summary>
+    public partial class UserControlExample : Control
+    {
+        public UserControlExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
