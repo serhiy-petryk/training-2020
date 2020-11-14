b159db6f6f8e8901310131ee8bbc0b88b781de23
serhiy-petryk <sergei_petrik@yahoo.com>
2020-11-14 02:24:38 +0200

    investigation of DatePickerTextBox/TextBox

--- WPF/WpfInvestigate/ControlInvestigate/MainWindow.xaml.cs
+++ WPF/WpfInvestigate/ControlInvestigate/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ControlInvestigate.Common;
 using ControlInvestigate.Tests;
 
 namespace ControlInvestigate
@@ -13,6 +17,27 @@ namespace ControlInvestigate
             InitializeComponent();
         }
 
-        private void TempControl_OnClick(object sender, RoutedEventArgs e) => new TempControl().Show();
+        private void TempControl_OnClick(object sender, RoutedEventArgs e)
+        {
+            // new TempControl().Show();
+            var a1 = TestTextBox;
+            var aa1 = Tips.GetVisualChildren(a1).ToList();
+            var a2 = aa1.OfType<ScrollViewer>().First();
+            var aa2 = Tips.GetVisualParents(a2).ToArray();
+        }
+
+        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var a1 = sender as TextBox;
+            var aa1 = Tips.GetVisualChildren(a1).ToList();
+        }
+
+        private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var a1 = sender as TextBox;
+            var aa1 = Tips.GetVisualChildren(a1).ToList();
+            var a2 = aa1.OfType<ScrollViewer>().First();
+            var aa2 = Tips.GetVisualParents(a2).ToArray();
+        }
     }
 }
