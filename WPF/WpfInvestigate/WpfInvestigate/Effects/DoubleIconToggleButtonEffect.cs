672364a1364e35fd65375e79dd980a3bd8b87d51
serhiy-petryk <sergei_petrik@yahoo.com>
2020-11-17 07:04:24 +0200

    refactored icon support

--- WPF/WpfInvestigate/WpfInvestigate/Effects/DoubleIconToggleButtonEffect.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Effects/DoubleIconToggleButtonEffect.cs
@@ -14,6 +14,111 @@ namespace WpfInvestigate.Effects
 {
     public class DoubleIconToggleButtonEffect
     {
+        public static readonly DependencyProperty PathOnProperty = DependencyProperty.RegisterAttached("PathOn",
+            typeof(Path), typeof(DoubleIconToggleButtonEffect), new UIPropertyMetadata(null, OnPathPropertyChanged));
+        public static Path GetPathOn(DependencyObject obj) => (Path)obj.GetValue(PathOnProperty);
+        public static void SetPathOn(DependencyObject obj, Path value) => obj.SetValue(PathOnProperty, value);
+        //=============
+        public static readonly DependencyProperty PathOffProperty = DependencyProperty.RegisterAttached("PathOff",
+            typeof(Path), typeof(DoubleIconToggleButtonEffect), new UIPropertyMetadata(null, OnPathPropertyChanged));
+        public static Path GetPathOff(DependencyObject obj) => (Path)obj.GetValue(PathOffProperty);
+        public static void SetPathOff(DependencyObject obj, Path value) => obj.SetValue(PathOffProperty, value);
+        //==============
+        public static readonly DependencyProperty ScaleOnProperty = DependencyProperty.RegisterAttached(
+            "ScaleOn", typeof(Size), typeof(DoubleIconToggleButtonEffect), new UIPropertyMetadata(Size.Empty, OnPathPropertyChanged));
+        public static Size GetScaleOn(DependencyObject obj) => (Size)obj.GetValue(ScaleOnProperty);
+        public static void SetScaleOn(DependencyObject obj, Size value) => obj.SetValue(ScaleOnProperty, value);
+        //==============
+        public static readonly DependencyProperty ScaleOffProperty = DependencyProperty.RegisterAttached(
+            "ScaleOff", typeof(Size), typeof(DoubleIconToggleButtonEffect), new UIPropertyMetadata(Size.Empty, OnPathPropertyChanged));
+        public static Size GetScaleOff(DependencyObject obj) => (Size)obj.GetValue(ScaleOffProperty);
+        public static void SetScaleOff(DependencyObject obj, Size value) => obj.SetValue(ScaleOffProperty, value);
+
+        private static void OnPathPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ToggleButton tb)
+            {
+                Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                {
+                    if ((!(tb.Content is FrameworkElement element) || !(GetViewbox(tb) is Viewbox)) && GetPathOn(tb) != null && GetPathOff(tb) != null)
+                    {
+                        var a1 = GetPathOn(tb).Data;
+                        var a2 = GetPathOff(tb).Data;
+
+                        Init(tb);
+                        tb.Checked -= OnToggleButtonCheckChanged;
+                        tb.Unchecked -= OnToggleButtonCheckChanged;
+                        tb.Checked += OnToggleButtonCheckChanged;
+                        tb.Unchecked += OnToggleButtonCheckChanged;
+                    }
+                }));
+            }
+        }
+
+        private static void Init(ToggleButton tb)
+        {
+            //ControlHelper.AddIconToControl(tb, false, tb.IsChecked == true ? GetGeometryOn(tb) : GetGeometryOff(tb),
+              //  tb.IsChecked == true ? GetMarginOn(tb) : GetMarginOff(tb), GetWidth(tb));
+            ControlHelper.AddPathToControl(tb, tb.IsChecked == true ? GetPathOn(tb) : GetPathOff(tb),
+                tb.IsChecked == true ? GetScaleOn(tb) : GetScaleOff(tb), false);
+        }
+
+        private static void OnToggleButtonCheckChanged(object sender, RoutedEventArgs e)
+        {
+            var tb = (ToggleButton)sender;
+            var viewbox = GetViewbox(tb);
+            if (viewbox == null) return;
+
+            var newPath = tb.IsChecked == true ? GetPathOn(tb) : GetPathOff(tb);
+            var oldScale = tb.IsChecked == true ? GetScaleOff(tb) : GetScaleOn(tb);
+            if (oldScale.IsEmpty)
+                oldScale = new Size(1.0, 1.0);
+            
+            var newScale = tb.IsChecked == true ? GetScaleOn(tb) : GetScaleOff(tb);
+            if (newScale.IsEmpty)
+                newScale = new Size(1.0, 1.0);
+
+            var storyboard = (Storyboard)viewbox.Resources["Animation"] ?? CreateAnimation(viewbox);
+            var firstAnimation = ((DoubleAnimation) storyboard.Children[0]);
+            firstAnimation.CurrentStateInvalidated += AnimationStateHandler;
+            firstAnimation.From = oldScale.Width;
+            ((DoubleAnimation)storyboard.Children[1]).To = newScale.Width;
+            storyboard?.Begin();
+
+            void AnimationStateHandler(object sender2, EventArgs args2)
+            {
+                firstAnimation.CurrentStateInvalidated -= AnimationStateHandler;
+                var clock = (AnimationClock) sender2;
+                if (clock.CurrentState == ClockState.Filling)
+                {
+                    viewbox.Child = newPath;
+                    ((ScaleTransform) viewbox.RenderTransform).ScaleX = 0.0;
+                }
+            }
+        }
+
+        private static Storyboard CreateAnimation(Viewbox viewbox)
+        {
+            if (!(viewbox.RenderTransform is ScaleTransform))
+                viewbox.RenderTransform = new ScaleTransform(1, 1);
+
+            var a1 = viewbox.CreateAnimation(new PropertyPath("RenderTransform.ScaleX"), typeof(double));
+            a1.SetFromToValues(0.0, 0.0);
+            var a2 = viewbox.CreateAnimation(new PropertyPath("RenderTransform.ScaleX"), typeof(double));
+            a2.BeginTime = a1.Duration.TimeSpan;
+            a2.SetFromToValues(0.0, 0.0);
+            
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(a1);
+            storyboard.Children.Add(a2);
+
+            viewbox.Resources.Add("Animation", storyboard);
+            return storyboard;
+        }
+
+        //=============================
+        //=========  OLD  =============
+        //=============================
         public static readonly DependencyProperty GeometryOnProperty = DependencyProperty.RegisterAttached("GeometryOn",
             typeof(Geometry), typeof(DoubleIconToggleButtonEffect), new UIPropertyMetadata(Geometry.Empty, OnGeometryPropertyChanged));
         public static Geometry GetGeometryOn(DependencyObject obj) => (Geometry)obj.GetValue(GeometryOnProperty);
@@ -44,7 +149,7 @@ namespace WpfInvestigate.Effects
         {
             Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() =>
             {
-                if (d is ToggleButton tb && GetViewbox(tb) is Viewbox viewbox)
+                if (d is ToggleButton tb && XXGetViewbox(tb) is Viewbox viewbox)
                     viewbox.Width = (double)e.NewValue;
             }));
         }
@@ -52,7 +157,7 @@ namespace WpfInvestigate.Effects
         {
             Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() =>
             {
-                if (d is ToggleButton tb && (GetViewbox(tb) is Viewbox viewbox) &&
+                if (d is ToggleButton tb && (XXGetViewbox(tb) is Viewbox viewbox) &&
                     ((tb.IsChecked == true && e.Property == MarginOnProperty) ||
                      (tb.IsChecked != null && e.Property == MarginOffProperty)))
                     viewbox.Margin = (Thickness)e.NewValue;
@@ -65,40 +170,41 @@ namespace WpfInvestigate.Effects
             {
                 Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
                 {
-                    if ((!(tb.Content is FrameworkElement element) || !(GetViewbox(tb) is Viewbox)) && GetGeometryOn(tb) != Geometry.Empty && GetGeometryOff(tb) != Geometry.Empty)
+                    if ((!(tb.Content is FrameworkElement element) || !(XXGetViewbox(tb) is Viewbox)) && GetGeometryOn(tb) != Geometry.Empty && GetGeometryOff(tb) != Geometry.Empty)
                     {
-                        Init(tb);
-                        tb.Checked -= OnToggleButtonCheckChanged;
-                        tb.Unchecked -= OnToggleButtonCheckChanged;
-                        tb.Checked += OnToggleButtonCheckChanged;
-                        tb.Unchecked += OnToggleButtonCheckChanged;
+                        XXInit(tb);
+                        tb.Checked -= XXOnToggleButtonCheckChanged;
+                        tb.Unchecked -= XXOnToggleButtonCheckChanged;
+                        tb.Checked += XXOnToggleButtonCheckChanged;
+                        tb.Unchecked += XXOnToggleButtonCheckChanged;
                     }
                 }));
             }
         }
 
-        private static void Init(ToggleButton tb)
+        private static void XXInit(ToggleButton tb)
         {
             ControlHelper.AddIconToControl(tb, false, tb.IsChecked == true ? GetGeometryOn(tb) : GetGeometryOff(tb),
                 tb.IsChecked == true ? GetMarginOn(tb) : GetMarginOff(tb), GetWidth(tb));
         }
 
-        private static void OnToggleButtonCheckChanged(object sender, RoutedEventArgs e)
+        private static void XXOnToggleButtonCheckChanged(object sender, RoutedEventArgs e)
         {
             var tb = (ToggleButton)sender;
             var newGeometry = tb.IsChecked == true ? GetGeometryOn(tb) : GetGeometryOff(tb);
             var newMargin = tb.IsChecked == true ? GetMarginOn(tb) : GetMarginOff(tb);
-            var sb = GetAnimation(GetViewbox(tb), newGeometry, newMargin);
+            var sb = XXGetAnimation(XXGetViewbox(tb), newGeometry, newMargin);
             sb?.Begin();
         }
 
         //============= Animation service ===================
-        private static Storyboard CreateAnimation(Viewbox viewbox)
+        private static Storyboard XXCreateAnimation(Viewbox viewbox)
         {
             var path = (Path)viewbox.Child;
             if (!(viewbox.RenderTransform is ScaleTransform))
                 viewbox.RenderTransform = new ScaleTransform(1, 1);
-            ((ScaleTransform)viewbox.RenderTransform).CenterX = viewbox.ActualWidth / 2;
+            // ((ScaleTransform)viewbox.RenderTransform).CenterX = viewbox.ActualWidth / 2;
+            viewbox.RenderTransformOrigin = new Point(0.5, 0.5);
 
             var a1 = viewbox.CreateAnimation(new PropertyPath("RenderTransform.ScaleX"), typeof(double));
             a1.SetFromToValues(1.0, 0.0);
@@ -116,14 +222,14 @@ namespace WpfInvestigate.Effects
             return storyboard;
         }
 
-        private static Storyboard GetAnimation(Viewbox viewbox, Geometry newGeometry, Thickness newMargin)
+        private static Storyboard XXGetAnimation(Viewbox viewbox, Geometry newGeometry, Thickness newMargin)
         {
             if (viewbox == null) return null;
 
             var storyboard = (Storyboard)viewbox.Resources["Animation"];
             if (storyboard == null)
             {
-                storyboard = CreateAnimation(viewbox);
+                storyboard = XXCreateAnimation(viewbox);
                 if (storyboard == null)
                     return null;
             }
@@ -134,6 +240,7 @@ namespace WpfInvestigate.Effects
         }
 
         // private static Viewbox GetViewbox(Grid grid) => grid.Children.OfType<Viewbox>().FirstOrDefault(vb => Grid.GetColumn(vb) == 1);
-        private static Viewbox GetViewbox(ToggleButton tb) => Tips.GetVisualChildren(tb).OfType<Viewbox>().FirstOrDefault(vb => vb.Resources["IconViewBox"] is bool);
+        private static Viewbox XXGetViewbox(ToggleButton tb) => Tips.GetVisualChildren(tb).OfType<Viewbox>().FirstOrDefault(vb => vb.Resources["IconViewBox"] is bool);
+        private static Viewbox GetViewbox(ToggleButton tb) => Tips.GetVisualChildren(tb).OfType<Viewbox>().FirstOrDefault(vb => vb.Resources["PathViewBox"] is bool);
     }
 }
--- WPF/WpfInvestigate/WpfInvestigate/Effects/IconEffect.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Effects/IconEffect.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using WpfInvestigate.Common;
 using WpfInvestigate.Controls.Helpers;
 
 namespace WpfInvestigate.Effects
@@ -50,93 +47,12 @@ namespace WpfInvestigate.Effects
         //======================
         private static void OnPropertiesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (!(d is ContentControl control) || !(e.NewValue is Path path)) return;
+            if (!(d is ContentControl control) || !(e.NewValue is Path && e.Property == PathProperty ||
+                                                    e.NewValue is Size && e.Property == ScaleProperty)) return;
 
-            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() =>
-            {
-                // AddPathToControl(control, path, true, control.HasContent ? GetMarginIfHasContent(control) : control.Padding);
-                SetPathInControl(control);
-            }));
+            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() => 
+                ControlHelper.AddPathToControl(control, GetPath(control), GetScale(control), true)));
         }
-
-        private static void SetScaleInViewbox(FrameworkElement viewbox, Size scale)
-        {
-            if (scale.IsEmpty)
-            {
-                viewbox.RenderTransform = Transform.Identity;
-            }
-            else
-            {
-                viewbox.RenderTransformOrigin = new Point(0.5, 0.5);
-                viewbox.RenderTransform = new ScaleTransform(scale.Width, scale.Height);
-            }
-        }
-        private static void SetPathInControl(ContentControl control, bool iconBeforeContent = true)
-        {
-            var path = GetPath(control);
-            var scale = GetScale(control);
-
-            if (control.Resources["AddedPath"] is bool)
-            { // icon already exists
-                var oldViewBox = Tips.GetVisualChildren(control).OfType<Viewbox>().FirstOrDefault(vb => vb.Resources["PathViewBox"] is bool);
-                if (oldViewBox != null)
-                {
-                    // wrong Margin/Width, якщо повторно виконуємо метод => потрібно ускладнити обробку (??? чи це потрібно)
-                    // oldViewBox.Margin = iconMargin;
-                    // oldViewBox.Width = iconWidth;
-                    if (oldViewBox.Child is Path oldPath && oldPath != path)
-                        oldViewBox.Child = path;
-                    SetScaleInViewbox(path, scale);
-                }
-                return;
-            }
-            control.Resources["AddedPath"] = true;
-
-            // var path = new Path { Stretch = Stretch.Uniform, Margin = new Thickness(), Data = icon };
-            /*path.SetBinding(Shape.FillProperty, new Binding("Foreground")
-            {
-                RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(Control), 1)
-            });*/
-            var viewbox = new Viewbox
-            {
-                Child = path,
-                VerticalAlignment = VerticalAlignment.Stretch,
-                // Margin = iconMargin,
-                Resources = { ["PathViewBox"] = true }
-            };
-
-            if (control.HasContent)
-            {
-                var grid = new Grid { ClipToBounds = true, Margin = new Thickness(), SnapsToDevicePixels = true };
-                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = iconBeforeContent ? GridLength.Auto : new GridLength(1, GridUnitType.Star) });
-                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = !iconBeforeContent ? GridLength.Auto : new GridLength(1, GridUnitType.Star) });
-
-                grid.Children.Add(viewbox);
-                Grid.SetColumn(viewbox, iconBeforeContent ? 0 : 1);
-
-                var contentControl = new ContentPresenter
-                {
-                    Content = control.Content,
-                    Margin = control.Padding,
-                    VerticalAlignment = control.VerticalContentAlignment,
-                    HorizontalAlignment = control.HorizontalContentAlignment
-                };
-                control.Content = null;
-                control.Padding = new Thickness();
-                // tb.VerticalContentAlignment = VerticalAlignment.Stretch;
-                control.HorizontalContentAlignment = HorizontalAlignment.Stretch;
-                grid.Children.Add(contentControl);
-                Grid.SetColumn(contentControl, iconBeforeContent ? 1 : 0);
-
-                control.Content = grid;
-            }
-            else
-                control.Content = viewbox;
-
-            SetScaleInViewbox(path, scale);
-        }
-
-
     }
 }
 
