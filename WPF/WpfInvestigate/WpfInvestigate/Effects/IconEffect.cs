cda8f99572a3f6b4e8d33aaa20231124a06d76a3
serhiy-petryk <sergei_petrik@yahoo.com>
2020-11-18 05:48:15 +0200

    revert to "BEFORE refactored effects:Icon" (2020-11-16)

--- WPF/WpfInvestigate/WpfInvestigate/Effects/IconEffect.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Effects/IconEffect.cs
@@ -2,7 +2,6 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using WpfInvestigate.Controls.Helpers;
 
@@ -12,29 +11,19 @@ namespace WpfInvestigate.Effects
     /// </summary>
     public class IconEffect
     {
-        public static readonly DependencyProperty PathProperty = DependencyProperty.RegisterAttached(
-            "Path", typeof(Path), typeof(IconEffect), new UIPropertyMetadata(null, OnPropertiesChanged));
-        public static Path GetPath(DependencyObject obj) => (Path)obj.GetValue(PathProperty);
-        public static void SetPath(DependencyObject obj, Path value) => obj.SetValue(PathProperty, value);
-
-        public static readonly DependencyProperty ScaleProperty = DependencyProperty.RegisterAttached(
-            "Scale", typeof(Size), typeof(IconEffect), new UIPropertyMetadata(Size.Empty, OnPropertiesChanged));
-        public static Size GetScale(DependencyObject obj) => (Size)obj.GetValue(ScaleProperty);
-        public static void SetScale(DependencyObject obj, Size value) => obj.SetValue(ScaleProperty, value);
-
-        //================
         public static readonly DependencyProperty GeometryProperty = DependencyProperty.RegisterAttached(
-            "Geometry", typeof(Geometry), typeof(IconEffect), new UIPropertyMetadata(null, OnGeometryPropertiesChanged));
+            "Geometry", typeof(Geometry), typeof(IconEffect), new UIPropertyMetadata(null, OnPropertiesChanged));
+
         public static Geometry GetGeometry(DependencyObject obj) => (Geometry)obj.GetValue(GeometryProperty);
         public static void SetGeometry(DependencyObject obj, Geometry value) => obj.SetValue(GeometryProperty, value);
 
         //==================
         public static readonly DependencyProperty MarginIfHasContentProperty = DependencyProperty.RegisterAttached("MarginIfHasContent",
-            typeof(Thickness), typeof(IconEffect), new UIPropertyMetadata(new Thickness(), OnGeometryPropertiesChanged));
+            typeof(Thickness), typeof(IconEffect), new UIPropertyMetadata(new Thickness(), OnPropertiesChanged));
         public static Thickness GetMarginIfHasContent(DependencyObject obj) => (Thickness)obj.GetValue(MarginIfHasContentProperty);
         public static void SetMarginIfHasContent(DependencyObject obj, Thickness value) => obj.SetValue(MarginIfHasContentProperty, value);
         //==================
-        private static void OnGeometryPropertiesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnPropertiesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (!(d is ContentControl control) || !(e.NewValue is Geometry geometry)) return;
 
@@ -42,16 +31,6 @@ namespace WpfInvestigate.Effects
             {
                 ControlHelper.AddIconToControl(control, true, geometry, control.HasContent ? GetMarginIfHasContent(control) : control.Padding);
             }));
-        }
-
-        //======================
-        private static void OnPropertiesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (!(d is ContentControl control) || !(e.NewValue is Path && e.Property == PathProperty ||
-                                                    e.NewValue is Size && e.Property == ScaleProperty)) return;
-
-            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() => 
-                ControlHelper.AddPathToControl(control, GetPath(control), GetScale(control), true)));
         }
     }
 }
