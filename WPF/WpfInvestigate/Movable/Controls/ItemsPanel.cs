4f0cad1b25d05b9c0b6e2f19aba5322e1741fd17
serhiy-petryk <sergei_petrik@yahoo.com>
2020-11-29 01:17:22 +0200

    x

---
+++ WPF/WpfInvestigate/Movable/Controls/ItemsPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Movable.Controls
+{
+    public class ItemsPanel: Canvas
+    {
+        protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
+        {
+            // Track when objects are added and removed
+            if (visualAdded != null)
+            {
+                // Do stuff with the added object
+            }
+            if (visualRemoved != null)
+            {
+                // Do stuff with the removed object
+            }
+
+            // Call base function
+            base.OnVisualChildrenChanged(visualAdded, visualRemoved);
+        }
+    }
+}
---
+++ WPF/WpfInvestigate/Movable/MainWindowItemsControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using WpfInvestigate.Samples;
+
+namespace Movable
+{
+    /// <summary>
+    /// Interaction logic for MainWindowItemsControl.xaml
+    /// </summary>
+    public partial class MainWindowItemsControl : Window
+    {
+        public MainWindowItemsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void AddChildToItemsControl_OnClick(object sender, RoutedEventArgs e)
+        {
+            var a1 = new SampleDialogMovable();
+            ItemsControlCanvas.Items.Add(a1);
+        }
+    }
+}
