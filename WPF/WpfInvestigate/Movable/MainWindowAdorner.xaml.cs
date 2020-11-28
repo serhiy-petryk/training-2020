eba2c08a55d4207287ed38d2e3d54d75943cbeab
serhiy-petryk <sergei_petrik@yahoo.com>
2020-11-28 22:17:54 +0200

    x

---
+++ WPF/WpfInvestigate/Movable/MainWindowAdorner.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Movable
+{
+    /// <summary>
+    /// Interaction logic for MainWindowAdorner.xaml
+    /// </summary>
+    public partial class MainWindowAdorner : Window
+    {
+        public MainWindowAdorner()
+        {
+            InitializeComponent();
+        }
+
+        private void AddAdorner_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
