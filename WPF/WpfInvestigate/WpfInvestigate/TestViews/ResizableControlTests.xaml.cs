15c07049f9d9a20033680c08da9497eb29740979
serhiy-petryk <sergei_petrik@yahoo.com>
2020-12-29 03:09:51 +0200

    added ResizableControlTests

---
+++ WPF/WpfInvestigate/WpfInvestigate/TestViews/ResizableControlTests.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfInvestigate.TestViews
+{
+    /// <summary>
+    /// Interaction logic for ResizableControl.xaml
+    /// </summary>
+    public partial class ResizableControlTests : Window
+    {
+        public ResizableControlTests()
+        {
+            InitializeComponent();
+        }
+    }
+}
