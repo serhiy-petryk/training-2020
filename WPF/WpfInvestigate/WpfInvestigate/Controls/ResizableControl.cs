5ae591b76516ccc59d355fc25764be5c2c737ba7
serhiy-petryk <sergei_petrik@yahoo.com>
2020-12-30 15:54:39 +0200

    WiP: ResizableControl

--- WPF/WpfInvestigate/WpfInvestigate/Controls/ResizableControl.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/ResizableControl.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Media;
 using WpfInvestigate.Common;
 
@@ -10,6 +11,9 @@ namespace WpfInvestigate.Controls
 {
     public class ResizableControl : ContentControl
     {
+        private static int Unique = 0;
+        private Panel HostPanel => VisualTreeHelper.GetParent(this) as Panel;
+
         public ResizableControl()
         {
             DataContext = this;
@@ -32,15 +36,100 @@ namespace WpfInvestigate.Controls
             }
         }
 
+        #region ==========  Focus  ============
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            Panel.SetZIndex(this, Unique++);
+        }
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+            if (Focusable)
+                Focus();
+        }
+
+
+        #endregion
+
         #region ==========  Moving && resizing  =========
         private void Thumb_OnDragStarted(object sender, DragStartedEventArgs e)
         {
-            if (!Focusable)
+            if (Focusable)
                 Focus();
             e.Handled = true;
         }
 
         private void MoveThumb_OnDragDelta(object sender, DragDeltaEventArgs e)
+        {
+            var aa1 = Tips.GetVisualParents(this).OfType<FrameworkElement>().ToArray();
+            var aa2 = Tips.GetVisualParents(this).OfType<FrameworkElement>().Select(a => new Size(a.ActualWidth, a.ActualHeight)).ToArray();
+
+            /*var itemsPresenter = Tips.GetVisualParents(this).OfType<ItemsPresenter>().FirstOrDefault();
+            var container = itemsPresenter == null ? null : VisualTreeHelper.GetParent(itemsPresenter) as FrameworkElement;
+            if (itemsPresenter != null && container != null)
+            {
+                var newX = itemsPresenter.Margin.Left + e.HorizontalChange;
+                if (newX + ActualWidth > container.ActualWidth)
+                    newX = container.ActualWidth - ActualWidth;
+                if (newX < 0) newX = 0;
+
+                var newY = itemsPresenter.Margin.Top + e.VerticalChange;
+                if (newY + ActualHeight > container.ActualHeight)
+                    newY = container.ActualHeight - ActualHeight;
+                if (newY < 0) newY = 0;
+
+                itemsPresenter.Margin = new Thickness { Left = newX, Top = newY };
+                e.Handled = true;
+            }
+            else */
+            if (HostPanel is Grid grid)
+                GridMoveThumb_OnDragDelta(grid, e);
+            else if (HostPanel is Canvas canvas)
+                CanvasMoveThumb_OnDragDelta(canvas, e);
+        }
+
+
+        private void GridMoveThumb_OnDragDelta(Grid grid, DragDeltaEventArgs e)
+        {
+                var newX = Margin.Left + e.HorizontalChange;
+                //if (newX + ActualWidth > HostPanel.ActualWidth)
+                //  newX = HostPanel.ActualWidth - ActualWidth;
+                //if (newX < 0) newX = 0;
+
+                var newY = Margin.Top + e.VerticalChange;
+                //if (newY + ActualHeight > HostPanel.ActualHeight)
+                //  newY = HostPanel.ActualHeight - ActualHeight;
+                // if (newY < 0) newY = 0;
+
+                Margin = new Thickness { Left = newX, Top = newY };
+                e.Handled = true;
+        }
+
+        private void CanvasMoveThumb_OnDragDelta(Canvas canvas, DragDeltaEventArgs e)
+        {
+            // var newX = Canvas.GetLeft(this) + e.HorizontalChange; // GetLeft/Top may be NaN
+            var p = TranslatePoint(new Point(0, 0), canvas);
+            var newX = p.X + e.HorizontalChange;
+            //if (newX + ActualWidth > HostPanel.ActualWidth)
+            //  newX = HostPanel.ActualWidth - ActualWidth;
+            //if (newX < 0) newX = 0;
+
+            // var newY = Canvas.GetTop(this) + e.VerticalChange;
+            var newY = p.Y + e.VerticalChange;
+            //if (newY + ActualHeight > HostPanel.ActualHeight)
+            //  newY = HostPanel.ActualHeight - ActualHeight;
+            //if (newY < 0) newY = 0;
+
+            Canvas.SetLeft(this, newX);
+            Canvas.SetTop(this, newY);
+            e.Handled = true;
+        }
+
+
+
+        private void xMoveThumb_OnDragDelta(object sender, DragDeltaEventArgs e)
         {
             var itemsPresenter = Tips.GetVisualParents(this).OfType<ItemsPresenter>().FirstOrDefault();
             var container = itemsPresenter == null ? null : VisualTreeHelper.GetParent(itemsPresenter) as FrameworkElement;
@@ -79,7 +168,7 @@ namespace WpfInvestigate.Controls
             e.Handled = true;
         }
 
-        private void OnResizeLeft(double horizontalChange, FrameworkElement itemsPresenter)
+        private void xOnResizeLeft(double horizontalChange, FrameworkElement itemsPresenter)
         {
             if (itemsPresenter != null)
             {
@@ -95,6 +184,19 @@ namespace WpfInvestigate.Controls
                     itemsPresenter.Margin = new Thickness(itemsPresenter.Margin.Left + change, itemsPresenter.Margin.Top, 0, 0);
                 }
             }
+        }
+        private void OnResizeLeft(double horizontalChange, FrameworkElement itemsPresenter)
+        {
+            var container = itemsPresenter == null ? null : VisualTreeHelper.GetParent(itemsPresenter) as FrameworkElement;
+            var change = Math.Min(horizontalChange, ActualWidth - MinWidth);
+
+            if ((ActualWidth - change) < 0)
+                change = ActualWidth - MaxWidth;
+            if (container != null && (itemsPresenter.Margin.Left + ActualWidth - change) > container.ActualWidth)
+                change = itemsPresenter.Margin.Left + ActualWidth - container.ActualWidth;
+
+            if (!Tips.AreEqual(0.0, change))
+                Width = ActualWidth - change;
         }
         private void OnResizeTop(double verticalChange, FrameworkElement itemsPresenter)
         {
