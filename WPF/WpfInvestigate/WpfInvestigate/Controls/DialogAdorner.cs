d69d5b365d1b22e41d249c2a6c842e656a6c7f2d
serhiy-petryk <sergei_petrik@yahoo.com>
2021-01-11 02:24:01 +0200

    refactored DialogAdorner

---
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/DialogAdorner.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using WpfInvestigate.Common;
+
+namespace WpfInvestigate.Controls
+{
+
+    public class DialogAdorner : AdornerControl
+    {
+        //===============================
+        private static FrameworkElement GetAdornedElement(FrameworkElement host)
+        {
+            if (host == null)
+                host = Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive);
+            host = (host as Window)?.Content as FrameworkElement ?? host;
+
+            if (host == null)
+                throw new Exception("AdornerControl error! AdornedElement can't be null");
+            return host;
+        }
+
+        private static CommandBinding _closeCommand = new CommandBinding(ApplicationCommands.Close, (s, e1) =>
+        {
+            RemoveAdorner(Tips.GetVisualParents(s as UIElement).OfType<DialogAdorner>().FirstOrDefault()?._host);
+        });
+
+        //===============================
+        public bool CloseOnClickBackground { get; set; } = true;
+
+        private FrameworkElement _host;
+
+        public DialogAdorner(FrameworkElement content, FrameworkElement host = null) : base(GetAdornedElement(host))
+        {
+            if (content == null)
+                throw new ArgumentNullException(nameof(content));
+
+            _host = host ?? Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive);
+
+            var layer = AdornerLayer.GetAdornerLayer(AdornedElement);
+            if (layer == null)
+                throw new Exception("DialogAdorner constructor error! AdornerLevel of host can't be null");
+
+            layer.Add(this);
+
+            var panel = new Grid
+            {
+                Background = new SolidColorBrush(Common.ColorSpaces.ColorUtils.StringToColor("#77777777")),
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch
+            };
+            panel.MouseLeftButtonDown += Panel_MouseLeftButtonDown;
+            panel.Children.Add(content);
+
+            Child = panel;
+
+            if (_host is Window)
+            {
+                var hostMargin = (AdornedElement as FrameworkElement).Margin;
+                Margin = new Thickness(-hostMargin.Left, -hostMargin.Top, hostMargin.Left, hostMargin.Top);
+                AdornerSize = AdornerSizeType.Container;
+            }
+
+            //==========================
+            content.MouseLeftButtonDown += (s, e1) => e1.Handled = true;
+            content.CommandBindings.Add(_closeCommand);
+
+            content.Dispatcher.Invoke(DispatcherPriority.Render, new Action(() =>
+                content.Margin = new Thickness(Math.Max(0, (panel.ActualWidth - content.ActualWidth) / 2),
+                    Math.Max(0, (panel.ActualHeight - content.ActualHeight) / 2), 0, 0)));
+        }
+
+        private static void Panel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var adorner = Tips.GetVisualParents((FrameworkElement)sender).OfType<DialogAdorner>().FirstOrDefault();
+            if (adorner != null && adorner.CloseOnClickBackground)
+            {
+                // _closeCommand.Command.Execute(adorner.AdornedElement);
+                RemoveAdorner(adorner.AdornedElement);
+            }
+        }
+
+        private static void RemoveAdorner(UIElement host)
+        {
+            if (((host as Window)?.Content as FrameworkElement ?? host) is FrameworkElement target &&
+                AdornerLayer.GetAdornerLayer(target) is AdornerLayer layer)
+            {
+                foreach (var a in (layer.GetAdorners(target) ?? new Adorner[0]).ToArray().OfType<DialogAdorner>())
+                {
+                    if (a.Child is Grid panel)
+                    {
+                        panel.Children.RemoveRange(0, panel.Children.Count);
+                        panel.MouseLeftButtonDown -= Panel_MouseLeftButtonDown;
+                    }
+
+                    layer.Remove(a);
+                }
+            }
+        }
+    }
+}
--- WPF/WpfInvestigate/WpfInvestigate/TestViews/ResizingControlTests.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/TestViews/ResizingControlTests.xaml.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Threading;
-using WpfInvestigate.Common;
 using WpfInvestigate.Controls;
 using WpfInvestigate.Samples;
 
@@ -48,99 +41,14 @@ namespace WpfInvestigate.TestViews
                 LimitPositionToPanelBounds = true,
                 ToolTip = "Width/Height=150"
             };
-            // var a1 = new DialogControl(content){CloseOnClickBackground = false};
-            var a1 = new DialogAdorner(content) {CloseOnClickBackground = false};
+            var a1 = new DialogAdorner(content) {CloseOnClickBackground = true};
         }
 
-        private void XAddPanel_OnClick(object sender, RoutedEventArgs e)
+        private void OnClickDialogMessageBlock(object sender, RoutedEventArgs e)
         {
-            var adorner = CreateAdornerCore(this);
-            var resizingControl3 = new ResizingControl
-            {
-                Content = new ResizableSample { Width = double.NaN, Height = double.NaN },
-                Width = 150,
-                Height = 150,
-                LimitPositionToPanelBounds = true,
-                ToolTip = "Width/Height=150"
-            };
-
-            resizingControl3.MouseLeftButtonDown += (s, e1) => e1.Handled = true;
-            resizingControl3.CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, (s, e1) => RemoveAdorner(this)));
-
-            ((Grid)adorner.Child).Children.Add(resizingControl3);
-
-            resizingControl3.Dispatcher.Invoke(DispatcherPriority.Render, new Action(() =>
-            {
-                resizingControl3.Margin = new Thickness
-                {
-                    Left = Math.Max(0, (adorner.Child.ActualWidth - resizingControl3.ActualWidth) / 2),
-                    Top = Math.Max(0, (adorner.Child.ActualHeight - resizingControl3.ActualHeight) / 2)
-                };
-            }));
+            var aa = MessageBlock.ShowDialog("Message text Message text Message text Message text Message text Message text ",
+                "Show Dialog", null, new[] { "OK", "Cancel", "Right", "Left" });
         }
 
-        private static void RemoveAdorner(FrameworkElement host)
-        {
-            if (((host as Window)?.Content as FrameworkElement ?? host) is FrameworkElement target &&
-                AdornerLayer.GetAdornerLayer(target) is AdornerLayer layer)
-            {
-                foreach (var a in (layer.GetAdorners(target) ?? new Adorner[0]).ToArray().OfType<AdornerControl>())
-                {
-                    if (a.Child is Grid panel)
-                    {
-                        panel.Children.RemoveRange(0, panel.Children.Count);
-                        panel.MouseLeftButtonDown -= Panel_MouseLeftButtonDown;
-                    }
-
-                    layer.Remove(a);
-                }
-            }
-        }
-
-        private static AdornerControl CreateAdornerCore(FrameworkElement host)
-        {
-            if (((host as Window)?.Content as FrameworkElement ?? host) is FrameworkElement target && AdornerLayer.GetAdornerLayer(target) is AdornerLayer layer)
-            {
-                var panel = new Grid
-                {
-                    Background = new SolidColorBrush(Common.ColorSpaces.ColorUtils.StringToColor("#77777777")),
-                    HorizontalAlignment = HorizontalAlignment.Stretch,
-                    VerticalAlignment = VerticalAlignment.Stretch
-                };
-
-                panel.MouseLeftButtonDown += Panel_MouseLeftButtonDown;
-
-                // Since there is no Adorner for the dialog, create a new one and set and return it.
-                var adorner = new AdornerControl(target);
-                adorner.Child = panel;
-
-                // If Adorner is set for Window, set margin to cancel Margin of Content element.
-                if (host is Window)
-                {
-                    adorner.Margin = new Thickness(-target.Margin.Left, -target.Margin.Top, target.Margin.Right, target.Margin.Bottom);
-                    adorner.AdornerSize = AdornerControl.AdornerSizeType.Container;
-                }
-
-                // If the target is Enable when the dialog is displayed, disable it only while the dialog is displayed.
-                /*if (target.IsEnabled)
-                {
-                    target.IsEnabled = false;
-                    // AllDialogClosed += (s, e) => target.IsEnabled = true;
-                }*/
-                // Added a process to remove Adorner when all dialogs are cleared
-                // AllDialogClosed += (s, e) => layer.Remove(adorner);
-                layer.Add(adorner);
-                return adorner;
-            }
-
-            return null;
-        }
-
-        private static void Panel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var adorner = Tips.GetVisualParents((FrameworkElement)sender).OfType<AdornerControl>().FirstOrDefault();
-            if (adorner?.AdornedElement is FrameworkElement target)
-                RemoveAdorner(target);
-        }
     }
 }
