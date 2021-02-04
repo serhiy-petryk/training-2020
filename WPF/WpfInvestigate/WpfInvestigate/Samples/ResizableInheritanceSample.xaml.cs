ae8c966c10cbf41dd50eb974324eb7f4134922e3
serhiy-petryk <sergei_petrik@yahoo.com>
2021-02-04 13:59:51 +0200

    refactored ResizableInheritanceSample

--- WPF/WpfInvestigate/WpfInvestigate/Samples/ResizableInheritanceSample.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Samples/ResizableInheritanceSample.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using WpfInvestigate.Common;
 
 namespace WpfInvestigate.Samples
 {
@@ -11,16 +15,37 @@ namespace WpfInvestigate.Samples
         {
             InitializeComponent();
             DataContext = this;
+            Loaded += ResizableInheritanceSample_Loaded;
+        }
+
+        private void ResizableInheritanceSample_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= ResizableInheritanceSample_Loaded;
+            //Dispatcher.Invoke(() =>
+            //{
+                var a1 = Template.FindName("MovingThumb", this);
+                var cp = Tips.GetVisualChildren(this).OfType<ContentPresenter>().FirstOrDefault();
+                var a2 = cp.ContentTemplate.FindName("MovingThumb", cp);
+                MovingThumb = a2 as Thumb;
+            //}, DispatcherPriority.DataBind);
         }
 
         // ==============================
-        public new static readonly DependencyProperty ContentProperty = DependencyProperty.Register(nameof(Content),
+        /*public new static readonly DependencyProperty ContentProperty = DependencyProperty.Register(nameof(Content),
             typeof(object), typeof(ResizableInheritanceSample), new UIPropertyMetadata(null));
         public new object Content
         {
             get => (object)GetValue(ContentProperty);
             set => SetValue(ContentProperty, value);
-        }
+        }*/
+
+        /*public static readonly DependencyProperty XXContentProperty = DependencyProperty.Register(nameof(XXContent),
+            typeof(object), typeof(ResizableInheritanceSample), new UIPropertyMetadata(null));
+        public object XXContent
+        {
+            get => (object)GetValue(XXContentProperty);
+            set => SetValue(XXContentProperty, value);
+        }*/
 
     }
 }
