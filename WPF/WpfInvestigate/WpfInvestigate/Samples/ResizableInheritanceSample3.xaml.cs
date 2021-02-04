efca958a34f927a32e9349698156a0dca25b0673
serhiy-petryk <sergei_petrik@yahoo.com>
2021-02-04 17:02:46 +0200

    refactored ResizableInheritanceSamples

--- WPF/WpfInvestigate/WpfInvestigate/Samples/ResizableInheritanceSample3.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Samples/ResizableInheritanceSample3.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
+using System.Windows;
 using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
 using WpfInvestigate.Common;
 
 namespace WpfInvestigate.Samples
@@ -7,6 +9,7 @@ namespace WpfInvestigate.Samples
     /// <summary>
     /// Interaction logic for ResizableInheritanceSample3.xaml
     /// </summary>
+    // [ContentProperty("XXContent")]
     public partial class ResizableInheritanceSample3
     {
         public ResizableInheritanceSample3()
@@ -21,22 +24,34 @@ namespace WpfInvestigate.Samples
             MovingThumb = Tips.GetVisualChildren(this).OfType<Thumb>().FirstOrDefault(t => t.Name == MovingThumbName);
         }
 
+        // ==============  Not need  ================
+        public new static readonly DependencyProperty XXContentProperty = DependencyProperty.Register(nameof(XXContent),
+            typeof(object), typeof(ResizableInheritanceSample3), new UIPropertyMetadata(null));
+        public new object XXContent
+        {
+            get => (object)GetValue(XXContentProperty);
+            set => SetValue(XXContentProperty, value);
+        }
+
         // ==============================
-        /*public new static readonly DependencyProperty ContentProperty = DependencyProperty.Register(nameof(Content),
+        public new static readonly DependencyProperty ContentProperty = DependencyProperty.Register(nameof(Content),
             typeof(object), typeof(ResizableInheritanceSample3), new UIPropertyMetadata(null));
         public new object Content
         {
             get => (object)GetValue(ContentProperty);
             set => SetValue(ContentProperty, value);
-        }*/
+        }
 
-        /*public static readonly DependencyProperty XXContentProperty = DependencyProperty.Register(nameof(XXContent),
-            typeof(object), typeof(ResizableInheritanceSample3), new UIPropertyMetadata(null));
-        public object XXContent
+        //=============================
+        public static readonly DependencyProperty BaseContentProperty = DependencyProperty.Register(nameof(BaseContent),
+            typeof(FrameworkElement), typeof(ResizableInheritanceSample3),
+            new UIPropertyMetadata(null, (o, args) => ((ResizableInheritanceSample3) o).SetBaseContent(args.NewValue)));
+        public FrameworkElement BaseContent
         {
-            get => (object)GetValue(XXContentProperty);
-            set => SetValue(XXContentProperty, value);
-        }*/
+            get => (FrameworkElement)GetValue(BaseContentProperty);
+            set => SetValue(BaseContentProperty, value);
+        }
+        private void SetBaseContent(object content) => base.Content = content;
 
     }
 }
