7475afb779f7eb6fde25aef918da9a13b63b2547
serhiy-petryk <sergei_petrik@yahoo.com>
2021-02-06 01:53:16 +0200

    added ResizableInheritanceSample5

---
+++ WPF/WpfInvestigate/WpfInvestigate/Samples/ResizableInheritanceSample5.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WpfInvestigate.Controls;
+
+namespace WpfInvestigate.Samples
+{
+    /// <summary>
+    /// Interaction logic for ResizableInheritanceSample5.xaml
+    /// </summary>
+    public partial class ResizableInheritanceSample5
+    {
+        public ResizableInheritanceSample5()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var rControl = Template.FindName("ResizingControl", this) as FrameworkElement;
+            if (rControl != null)
+                SetBinding(Panel.ZIndexProperty, new Binding("(Panel.ZIndex)") { Source = rControl });
+        }
+    }
+}
