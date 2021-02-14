07c72735a030677585a25aa20c4e60055052a3d1
serhiy-petryk <sergei_petrik@yahoo.com>
2021-02-14 08:46:08 +0200

    added MwiContainer.LeftPanel

---
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/MwiContainer.LeftPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace WpfInvestigate.Controls
+{
+    public partial class MwiContainer
+    {
+        public static readonly DependencyProperty LeftPanelProperty = DependencyProperty.Register("LeftPanel", typeof(FrameworkElement), typeof(MwiContainer), new FrameworkPropertyMetadata(null, LeftPanel_OnPropertyChanged));
+
+        public FrameworkElement LeftPanel
+        {
+            get => (FrameworkElement)GetValue(LeftPanelProperty);
+            set => SetValue(LeftPanelProperty, value);
+        }
+        private static void LeftPanel_OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+        }
+    }
+}
