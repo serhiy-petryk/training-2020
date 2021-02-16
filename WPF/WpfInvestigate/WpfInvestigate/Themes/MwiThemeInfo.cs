86e82c1b33f4951a2f172cea41532bedd3b31c1e
serhiy-petryk <sergei_petrik@yahoo.com>
2021-02-16 22:59:20 +0200

    added MwiAppViewModel

---
+++ WPF/WpfInvestigate/WpfInvestigate/Themes/MwiThemeInfo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+
+namespace WpfInvestigate.Themes
+{
+    public class MwiThemeInfo
+    {
+        public static MwiThemeInfo[] Themes =
+        {
+            new MwiThemeInfo("Windows10", "Стиль Windows 10", null, new[] { "/Themes/Wnd7.xaml"}),
+            new MwiThemeInfo("Windows7", "Стиль Windows 7", null, new[] { "/Themes/Wnd10.xaml"})
+            /*new MwiThemeInfo("Windows10", "Стиль Windows 10", null, new[] { "/Mwi/Themes/MwiChild.Wnd10.xaml", "/Themes/MwiContainer.Wnd7.xaml"}),
+            new MwiThemeInfo("Windows7", "Стиль Windows 7", null, new[] { "/Mwi/Themes/MwiChild.Wnd7.xaml", "/Themes/MwiContainer.Wnd7.xaml"})*/
+        };
+
+        // ==============   Static section  ======================
+        private static Dictionary<string, Assembly> _assemblyCache = new Dictionary<string, Assembly>();
+        // Luna/Luna homestead/Royale are very similar themes
+
+        private static Assembly GetAssembly(string assemblyName)
+        {
+            var curAsm = Assembly.GetExecutingAssembly();
+            if (!_assemblyCache.ContainsKey(assemblyName))
+            {
+                var resource = assemblyName;
+                using (var stm = curAsm.GetManifestResourceStream(resource))
+                {
+                    var ba = new byte[(int)stm.Length];
+                    stm.Read(ba, 0, (int)stm.Length);
+                    _assemblyCache.Add(assemblyName, Assembly.Load(ba));
+                }
+            }
+            return _assemblyCache[assemblyName];
+        }
+
+        // ==============   Instance section  ======================
+        public string Id { get; }
+        public string Name { get; }
+
+        private string _assemblyName;
+        private string[] _uris;
+        private ResourceDictionary[] _resources = null;
+        public MwiThemeInfo(string id, string name, string assemblyName, string[] uris)
+        {
+            Id = id;
+            Name = name;
+            _assemblyName = assemblyName;
+            _uris = uris;
+        }
+
+        public void ApplyTheme()
+        {
+            if (!string.IsNullOrEmpty(_assemblyName) && _resources == null)
+            {
+                var assembly = GetAssembly(_assemblyName);
+                var uris = _uris.Select(uri => new Uri("/" + assembly.GetName().Name + ";component/" + uri, UriKind.Relative));
+                _resources = uris.Select(uri => Application.LoadComponent(uri) as ResourceDictionary).ToArray();
+            }
+            else if (_resources == null && _uris != null)
+                _resources = _uris.Select(uri => new ResourceDictionary { Source = new Uri(uri, UriKind.RelativeOrAbsolute) }).ToArray();
+            else if (_resources == null && _uris == null)
+                _resources = new ResourceDictionary[0];
+
+            // Clear old themes and add new theme
+            Application.Current.Resources.MergedDictionaries.Clear();
+            foreach (var r in _resources)
+                Application.Current.Resources.MergedDictionaries.Add(r);
+        }
+
+        public override string ToString() => Id;
+    }
+}
---
+++ WPF/WpfInvestigate/WpfInvestigate/ViewModels/MwiAppViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using WpfInvestigate.Common;
+using WpfInvestigate.Controls;
+using WpfInvestigate.Themes;
+
+namespace WpfInvestigate.ViewModels
+{
+    public class MwiAppViewModel: UIElement
+    {
+        //==========  Static section  ===========
+        public static MwiAppViewModel Instance = new MwiAppViewModel();
+
+        //=========================================
+        //==========  Instance section  ===========
+        public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register(nameof(ScaleValue), typeof(double), typeof(MwiAppViewModel), new UIPropertyMetadata(1.0));
+        public static readonly RoutedEvent ThemeChangedEvent = EventManager.RegisterRoutedEvent("ThemeChanged", RoutingStrategy.Bubble,
+                typeof(RoutedPropertyChangedEventHandler<MwiThemeInfo>), typeof(MwiAppViewModel));
+
+        public double ScaleValue
+        {
+            get => (double)GetValue(ScaleValueProperty);
+            set => SetValue(ScaleValueProperty, value);
+        }
+
+        public event RoutedPropertyChangedEventHandler<MwiThemeInfo> ThemeChanged
+        {
+            add => AddHandler(ThemeChangedEvent, value);
+            remove => RemoveHandler(ThemeChangedEvent, value);
+        }
+
+        public MwiContainer ContainerControl { get; set; }
+        public FontFamily DefaultFontFamily { get; } = new FontFamily("Segoe UI");
+        public Dock WindowsBarLocation { get; } = Dock.Top;
+        public RelayCommand CmdToggleScheme { get; }
+
+        private MwiThemeInfo _currentTheme;
+
+        public MwiAppViewModel() => CmdToggleScheme = new RelayCommand(ToggleTheme);
+
+        //=========================
+        private void ToggleTheme(object parameter)
+        {
+            var oldTheme = _currentTheme;
+            _currentTheme = MwiThemeInfo.Themes.First(t => t != _currentTheme);
+            _currentTheme.ApplyTheme();
+
+            var args = new RoutedPropertyChangedEventArgs<MwiThemeInfo>(oldTheme, _currentTheme) { RoutedEvent = ThemeChangedEvent };
+            RaiseEvent(args);
+        }
+    }
+}
