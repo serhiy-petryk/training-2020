ff19552212eecc5021b46de6f6c29da52444485d
serhiy-petryk <sergei_petrik@yahoo.com>
2021-02-16 23:46:46 +0200

    fixed Scale of Mwi

--- WPF/WpfInvestigate/WpfInvestigate/ViewModels/MwiAppViewModel.cs
+++ WPF/WpfInvestigate/WpfInvestigate/ViewModels/MwiAppViewModel.cs
@@ -1,39 +1,34 @@
 ﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 using WpfInvestigate.Common;
-using WpfInvestigate.Controls;
 using WpfInvestigate.Themes;
 
 namespace WpfInvestigate.ViewModels
 {
     public class MwiAppViewModel: UIElement
     {
-        //==========  Static section  ===========
+        #region ================  Static section  =====================
         public static MwiAppViewModel Instance = new MwiAppViewModel();
+        #endregion
 
-        //=========================================
-        //==========  Instance section  ===========
+        #region ================  Instance section  ====================
         public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register(nameof(ScaleValue), typeof(double), typeof(MwiAppViewModel), new UIPropertyMetadata(1.0));
-        public static readonly RoutedEvent ThemeChangedEvent = EventManager.RegisterRoutedEvent("ThemeChanged", RoutingStrategy.Bubble,
-                typeof(RoutedPropertyChangedEventHandler<MwiThemeInfo>), typeof(MwiAppViewModel));
-
         public double ScaleValue
         {
             get => (double)GetValue(ScaleValueProperty);
             set => SetValue(ScaleValueProperty, value);
         }
-
+        //=============================
+        public static readonly RoutedEvent ThemeChangedEvent = EventManager.RegisterRoutedEvent("ThemeChanged", RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<MwiThemeInfo>), typeof(MwiAppViewModel));
         public event RoutedPropertyChangedEventHandler<MwiThemeInfo> ThemeChanged
         {
             add => AddHandler(ThemeChangedEvent, value);
             remove => RemoveHandler(ThemeChangedEvent, value);
         }
-
-        public MwiContainer ContainerControl { get; set; }
-        public FontFamily DefaultFontFamily { get; } = new FontFamily("Segoe UI");
-        public Dock WindowsBarLocation { get; } = Dock.Top;
+        //=============================
+        // public MwiContainer ContainerControl { get; set; }
+        // public FontFamily DefaultFontFamily { get; } = new FontFamily("Segoe UI");
+        // public Dock WindowsBarLocation { get; } = Dock.Top;
         public RelayCommand CmdToggleScheme { get; }
 
         private MwiThemeInfo _currentTheme;
@@ -50,5 +45,6 @@ namespace WpfInvestigate.ViewModels
             var args = new RoutedPropertyChangedEventArgs<MwiThemeInfo>(oldTheme, _currentTheme) { RoutedEvent = ThemeChangedEvent };
             RaiseEvent(args);
         }
+        #endregion
     }
 }
