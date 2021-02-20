c2e70da8942c4eb516ac72a2f0920036ae0707f4
serhiy-petryk <sergei_petrik@yahoo.com>
2021-02-20 19:03:35 +0200

    fixed split of MwiBar

--- WPF/WpfInvestigate/WpfInvestigate/Controls/MwiBar.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/MwiBar.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
@@ -21,15 +22,56 @@ namespace WpfInvestigate.Controls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MwiBar), new FrameworkPropertyMetadata(typeof(MwiBar)));
         }
 
-        public MwiBar()
-        {
-            // InitializeComponent();
-        }
-
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
             _doubleButtonGrid = GetTemplateChild("DoubleButtonGrid") as Grid;
+            _scrollViewer = GetTemplateChild("PART_ScrollViewer") as ScrollViewer;
+            if (_scrollViewer != null)
+                _scrollViewer.ScrollChanged += TabScrollViewer_OnScrollChanged;
+        }
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+
+            var model = SelectedItem;
+            var item = ItemContainerGenerator.ContainerFromItem(model) as TabItem;
+            var scrollViewer = GetTemplateChild("PART_ScrollViewer") as ScrollViewer;
+            if (item != null && scrollViewer != null)
+            {
+                var point = item.TranslatePoint(new Point(), scrollViewer);
+                scrollViewer.ScrollToHorizontalOffset(point.X + (item.ActualWidth / 2));
+            }
+
+            foreach (var a1 in e.RemovedItems)
+                Dispatcher.InvokeAsync(() => AnimateTabButton((TabItem)ItemContainerGenerator.ContainerFromItem(a1)), DispatcherPriority.ContextIdle);
+
+            foreach (var a1 in e.AddedItems)
+                Dispatcher.InvokeAsync(() => AnimateTabButton((TabItem)ItemContainerGenerator.ContainerFromItem(a1)), DispatcherPriority.ContextIdle);
+        }
+
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        foreach (var item in e.NewItems)
+                            TabItem_AttachEvent(ItemContainerGenerator.ContainerFromItem(item) as TabItem, false);
+                        break;
+                    case NotifyCollectionChangedAction.Reset:
+                        foreach (var item in ItemsSource)
+                            TabItem_AttachEvent(ItemContainerGenerator.ContainerFromItem(item) as TabItem, false);
+                        break;
+                    case NotifyCollectionChangedAction.Remove:
+                        break;
+                    default: throw new Exception("Please, check code");
+                }
+            }), DispatcherPriority.Render);
         }
 
         public bool CanScrollLeft => _scrollableWidth >= Tips.SCREEN_TOLERANCE && !Tips.AreEqual(_horizontalOffset, 0);
@@ -40,6 +82,7 @@ namespace WpfInvestigate.Controls
                 ? Visibility.Collapsed : Visibility.Visible;
 
         private Grid _doubleButtonGrid;
+        private ScrollViewer _scrollViewer;
         private double _scrollableWidth;
         private double _viewportWidth;
         private double _extentWidth;
@@ -67,25 +110,36 @@ namespace WpfInvestigate.Controls
             }
         }
 
-        private void WindowsBar_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var model = SelectedItem;
-            var item = ItemContainerGenerator.ContainerFromItem(model) as TabItem;
-            var scrollViewer = GetTemplateChild("PART_ScrollBar") as ScrollViewer;
-            if (item != null && scrollViewer != null)
-            {
-                var point = item.TranslatePoint(new Point(), scrollViewer);
-                scrollViewer.ScrollToHorizontalOffset(point.X + (item.ActualWidth / 2));
-            }
-
-            foreach (var a1 in e.RemovedItems)
-                Dispatcher.InvokeAsync(() => AnimateTabButton((TabItem)ItemContainerGenerator.ContainerFromItem(a1)), DispatcherPriority.ContextIdle);
-
-            foreach (var a1 in e.AddedItems)
-                Dispatcher.InvokeAsync(() => AnimateTabButton((TabItem)ItemContainerGenerator.ContainerFromItem(a1)), DispatcherPriority.ContextIdle);
-        }
-
         #region ==============  Tab item  ==============
+        private void TabItem_AttachEvent(TabItem item, bool onlyDetach)
+        {
+            if (item == null) return;
+            var child = VisualTreeHelper.GetChildrenCount(item) > 0 ? VisualTreeHelper.GetChild(item, 0) as FrameworkElement : null;
+
+            item.Unloaded -= OnTabItemUnloaded;
+            item.PreviewMouseLeftButtonDown -= TabItem_OnPreviewMouseLeftButtonDown;
+            item.Loaded -= TabItem_OnLoaded;
+            item.MouseEnter -= TabItem_OnMouseEnterOrLeave;
+            item.MouseLeave -= TabItem_OnMouseEnterOrLeave;
+            if (child != null)
+                child.ToolTipOpening -= TabItem_OnToolTipOpening;
+            if (child != null && child.ToolTip is ToolTip childTooltip1)
+                childTooltip1.Opened -= TabToolTip_OnOpened;
+
+            if (onlyDetach) return;
+
+            item.Unloaded += OnTabItemUnloaded;
+            item.PreviewMouseLeftButtonDown += TabItem_OnPreviewMouseLeftButtonDown;
+            item.Loaded += TabItem_OnLoaded;
+            item.MouseEnter += TabItem_OnMouseEnterOrLeave;
+            item.MouseLeave += TabItem_OnMouseEnterOrLeave;
+            if (child != null)
+                child.ToolTipOpening += TabItem_OnToolTipOpening;
+            if (child != null && child.ToolTip is ToolTip childTooltip)
+                childTooltip.Opened += TabToolTip_OnOpened;
+
+            void OnTabItemUnloaded(object sender, RoutedEventArgs e) => TabItem_AttachEvent(sender as TabItem, true);
+        }
 
         private void TabItem_OnLoaded(object sender, RoutedEventArgs e) =>
             ((TabItem) sender).BeginAnimation(OpacityProperty, new DoubleAnimation(0.0, 1.0, AnimationHelper.AnimationDuration));
@@ -107,8 +161,7 @@ namespace WpfInvestigate.Controls
             e.Handled = true;
         }
 
-        private void TabItem_OnToolTipOpening(object sender, ToolTipEventArgs e) =>
-            ((MwiChild) ((FrameworkElement) sender).DataContext).RefreshThumbnail();
+        private void TabItem_OnToolTipOpening(object sender, ToolTipEventArgs e) => ((MwiChild)((FrameworkElement)sender).DataContext).RefreshThumbnail();
 
         private void TabToolTip_OnOpened(object sender, RoutedEventArgs e)
         {
@@ -119,12 +172,9 @@ namespace WpfInvestigate.Controls
 
         private void AnimateTabButton(TabItem tabItem)
         {
-            if (tabItem == null) // For VS designer
-                return;
+            if (tabItem == null) return; // For VS designer
 
             LinearGradientBrush newBrush;
-            // var dc = tabItem.DataContext as MwiChild;
-            // if (dc.IsSelected)
             if (tabItem.IsSelected)
                 newBrush = FindResource("Mwi.WindowTab.Selected.BackgroundBrush") as LinearGradientBrush;
             else if (tabItem.IsMouseOver)
