008d1ef4d486ef8881c27a43f4b2fb9e3bf8ffde
serhiy-petryk <sergei_petrik@yahoo.com>
2021-03-11 22:28:13 +0200

    added TestDll project

---
+++ WPF/WpfInvestigate/TestDll/TestViews/BootstrapButtonTests.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using WpfInvestigate.Effects;
+
+namespace TestDll.TestViews
+{
+    /// <summary>
+    /// Interaction logic for BootstrapButtonTests.xaml
+    /// </summary>
+    public partial class BootstrapButtonTests : Window
+    {
+        public BootstrapButtonTests()
+        {
+            InitializeComponent();
+        }
+
+        private int _hue;
+        private void Monochrome_ChangeColor_OnClick(object sender, RoutedEventArgs e)
+        {
+            _hue = (_hue + 30) % 360;
+            var color = new WpfInvestigate.Common.ColorSpaces.HSL(_hue / 360.0, 1, 0.4).RGB.Color;
+            ChromeEffect.SetMonochrome(MonochromeButtonWhite, color);
+            ChromeEffect.SetMonochrome(MonochromeButtonBlack, color);
+            ChromeEffect.SetMonochromeAnimated(MonochromeAnimatedButtonWhite, color);
+            ChromeEffect.SetMonochromeAnimated(MonochromeAnimatedButtonBlack, color);
+        }
+
+        private void Bichrome_ChangeColor_OnClick(object sender, RoutedEventArgs e)
+        {
+            _hue = (_hue + 30) % 360;
+            var color = new WpfInvestigate.Common.ColorSpaces.HSL(_hue / 360.0, 1, 0.4).RGB.Color;
+            ChromeEffect.SetBichromeBackground(BichromeButtonWhite, color);
+            ChromeEffect.SetBichromeBackground(BichromeButtonBlack, color);
+            ChromeEffect.SetBichromeAnimatedBackground(BichromeAnimatedButtonWhite, color);
+            ChromeEffect.SetBichromeAnimatedBackground(BichromeAnimatedButtonBlack, color);
+        }
+    }
+}
