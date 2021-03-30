1fd6fc4c2cf70b45d768315753d474d7cdd1b525
serhiy-petryk <sergei_petrik@yahoo.com>
2021-03-30 05:08:25 +0300

    refactored ChromeEffect -> removed effects without animation

--- WPF/WpfInvestigate/TestDll/TestViews/BootstrapButtonTests.xaml.cs
+++ WPF/WpfInvestigate/TestDll/TestViews/BootstrapButtonTests.xaml.cs
@@ -20,8 +20,6 @@ namespace TestDll.TestViews
             var color = new WpfInvestigate.Common.ColorSpaces.HSL(_hue / 360.0, 1, 0.4).RGB.Color;
             ChromeEffect.SetMonochrome(MonochromeButtonWhite, color);
             ChromeEffect.SetMonochrome(MonochromeButtonBlack, color);
-            ChromeEffect.SetMonochrome(MonochromeAnimatedButtonWhite, color);
-            ChromeEffect.SetMonochrome(MonochromeAnimatedButtonBlack, color);
         }
 
         private void Bichrome_ChangeColor_OnClick(object sender, RoutedEventArgs e)
@@ -30,8 +28,6 @@ namespace TestDll.TestViews
             var color = new WpfInvestigate.Common.ColorSpaces.HSL(_hue / 360.0, 1, 0.4).RGB.Color;
             ChromeEffect.SetBichromeBackground(BichromeButtonWhite, color);
             ChromeEffect.SetBichromeBackground(BichromeButtonBlack, color);
-            ChromeEffect.SetBichromeBackground(BichromeAnimatedButtonWhite, color);
-            ChromeEffect.SetBichromeBackground(BichromeAnimatedButtonBlack, color);
         }
     }
 }
