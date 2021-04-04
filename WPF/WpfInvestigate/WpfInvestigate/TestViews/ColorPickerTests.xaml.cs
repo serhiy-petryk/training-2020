d7ee2800137da04e4882d8af540f3d16f117412c
serhiy-petryk <sergei_petrik@yahoo.com>
2021-04-05 02:45:01 +0300

    removed MwiChild.XX.xaml

--- WPF/WpfInvestigate/WpfInvestigate/TestViews/ColorPickerTests.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/TestViews/ColorPickerTests.xaml.cs
@@ -17,18 +17,6 @@ namespace WpfInvestigate.TestViews
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             ColorControl.SaveColor();
-            /*var newRes = new ResourceDictionary();
-            newRes.Source = new Uri("pack://application:,,,/WpfInvestigate;component/Themes/MwiChild.Wnd10.xaml", UriKind.RelativeOrAbsolute);
-            newRes["Mwi.Child.BaseBackgroundColor"] = ColorControl.Color;
-            newRes["Mwi.Child.BaseBackgroundBrush"] = new SolidColorBrush(ColorControl.Color);
-            // this.Resources.MergedDictionaries.Clear();
-            // Resources["Mwi.Child.BaseBackgroundColor"] = ColorControl.Color;
-
-            var rd1 = new ResourceDictionary();
-            rd1.Source = new Uri("pack://application:,,,/WpfInvestigate;component/Themes/Mwi.Wnd10.xaml", UriKind.RelativeOrAbsolute);
-            // rd1["Mwi.Child.BaseBackgroundColor"] = ColorControl.Color;
-            rd1.MergedDictionaries.Clear();
-            rd1.MergedDictionaries.Add(newRes);*/
             MwiAppViewModel.Instance.AppColor = ColorControl.Color;
         }
 
