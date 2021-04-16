d60ac918ac3b43311a1ab3a2a1c078ec7446acc7
serhiy-petryk <sergei_petrik@yahoo.com>
2021-04-16 10:06:57 +0300

    refactored MemoryLeakInvestigation

--- WPF/WpfInvestigate/WpfInvestigate/TestViews/MemoryLeakInvestigation.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/TestViews/MemoryLeakInvestigation.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Data;
+using WpfInvestigate.Common;
 
 namespace WpfInvestigate.TestViews
 {
@@ -14,6 +21,45 @@ namespace WpfInvestigate.TestViews
 
         private void OnButtonClick(object sender, RoutedEventArgs e)
         {
+            // var a1 = System.Windows.Data.BindingOperations.GetBinding(Button, ContentProperty);
+            // BindingOperations.ClearAllBindings(Button);
+            // BindingOperations.ClearAllBindings(MwiBootstrapColorTests.Instance);
+            // Tips.ClearAllBindings(Button);
+            Tips.ClearAllBindings(MwiBootstrapColorTests.Instance);
         }
+
+        private static Dictionary<Type, List<FieldInfo>> _dpOfTypeCache = new Dictionary<Type, List<FieldInfo>>();
+        private static int _clearCount = 0;
+        public static void ClearAllBindings(DependencyObject target)
+            // based on 'H.B.' comment in https://stackoverflow.com/questions/5023025/is-there-a-way-to-get-all-bindingexpression-objects-for-a-window
+        {
+            foreach (var child in (new[] { target }).Union(Tips.GetVisualChildren(target)))
+            {
+                var type = child.GetType();
+                if (!_dpOfTypeCache.ContainsKey(type))
+                {
+                    var propertiesDp = new List<FieldInfo>();
+                    var currentType = type;
+                    while (currentType != typeof(object))
+                    {
+                        propertiesDp.AddRange(currentType.GetFields().Where(x => x.FieldType == typeof(DependencyProperty)));
+                        currentType = currentType.BaseType;
+                    }
+                    _dpOfTypeCache[type] = propertiesDp;
+                }
+
+                _dpOfTypeCache[type].ForEach(dp =>
+                {
+                    var dp1 = dp.GetValue(child) as DependencyProperty;
+                    var a1 = BindingOperations.GetBinding(child, dp1);
+                    if (a1 != null)
+                    {
+                        BindingOperations.ClearBinding(child, dp1);
+                        Debug.Print($"Child: {child}, {dp1.Name}, {a1}");
+                    }
+                });
+            }
+        }
+
     }
 }
