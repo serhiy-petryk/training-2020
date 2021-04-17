75b5db6519c4ab308d608752dcd7005a8c655f7e
serhiy-petryk <sergei_petrik@yahoo.com>
2021-04-17 22:37:30 +0300

    x

--- WPF/WpfInvestigate/WpfInvestigate/Controls/ResizingControl.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/ResizingControl.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Threading;
 using WpfInvestigate.Common;
 
 namespace WpfInvestigate.Controls
--- WPF/WpfInvestigate/WpfInvestigate/MainWindow.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/MainWindow.xaml.cs
@@ -7,7 +7,6 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using WpfInvestigate.Common;
 using WpfInvestigate.Helpers;
 using WpfInvestigate.Obsolete;
 using WpfInvestigate.Obsolete.TestViews;
