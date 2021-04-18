6be9cca38fe97cc273e0e24f0fa9d1ef2ba8819c
serhiy-petryk <sergei_petrik@yahoo.com>
2021-04-19 02:07:08 +0300

    refactored MwiBootstrapColorTests

--- WPF/WpfInvestigate/WpfInvestigate/TestViews/MemoryLeakInvestigation.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/TestViews/MemoryLeakInvestigation.xaml.cs
@@ -19,8 +19,8 @@ namespace WpfInvestigate.TestViews
             // BindingOperations.ClearAllBindings(Button);
             // BindingOperations.ClearAllBindings(MwiBootstrapColorTests.Instance);
             // Tips.ClearAllBindings(Button);
-            if (MwiBootstrapColorTests.Instance != null)
-                CleanerHelper.ClearAllBindings(MwiBootstrapColorTests.Instance);
+            // if (MwiBootstrapColorTests.Instance != null)
+               // CleanerHelper.ClearAllBindings(MwiBootstrapColorTests.Instance);
         }
     }
 }
--- WPF/WpfInvestigate/WpfInvestigate/TestViews/MwiBootstrapColorTests.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/TestViews/MwiBootstrapColorTests.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using WpfInvestigate.Common;
+using WpfInvestigate.Controls;
 
 namespace WpfInvestigate.TestViews
 {
@@ -10,6 +16,29 @@ namespace WpfInvestigate.TestViews
         public MwiBootstrapColorTests()
         {
             InitializeComponent();
+        }
+
+        private void OnAddWindowClick(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as ButtonBase;
+            var userControl = btn.GetVisualParents().OfType<UserControl>().First();
+            var container = btn.GetVisualParents().OfType<MwiContainer>().First();
+
+            var mwiChild = new MwiChild { Title = userControl.Tag.ToString() };
+
+            var b1 = new Binding {Path = new PropertyPath("Background"), Source = userControl};
+            mwiChild.SetBinding(BackgroundProperty, b1);
+
+            var b2 = new Binding
+            {
+                Path = new PropertyPath("Background"),
+                Source = userControl,
+                Converter = ColorHslBrush.Instance,
+                ConverterParameter = "+75%"
+            };
+            mwiChild.SetBinding(ForegroundProperty, b2);
+
+            container.Children.Add(mwiChild);
         }
     }
 }
