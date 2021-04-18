495880fe06895c038aa30e854cf4ad146f23f9e8
serhiy-petryk <sergei_petrik@yahoo.com>
2021-04-19 02:54:15 +0300

    added Test in MwiBootstrapColorTests to test memory leak

--- WPF/WpfInvestigate/WpfInvestigate/TestViews/MwiBootstrapColorTests.xaml.cs
+++ WPF/WpfInvestigate/WpfInvestigate/TestViews/MwiBootstrapColorTests.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -39,6 +42,52 @@ namespace WpfInvestigate.TestViews
             mwiChild.SetBinding(ForegroundProperty, b2);
 
             container.Children.Add(mwiChild);
+        }
+
+        private async void OnRunTestClick(object sender, RoutedEventArgs e)
+        {
+            for (var k = 0; k < 5; k++)
+                await StepOfTest(sender, k);
+        }
+
+        private async Task StepOfTest(object sender, int step)
+        {
+            var btn = sender as ButtonBase;
+            var userControl = btn.GetVisualParents().OfType<UserControl>().First();
+            var container = btn.GetVisualParents().OfType<MwiContainer>().First();
+
+            var mwiChild = new MwiChild { Title = userControl.Tag.ToString() };
+            var b1 = new Binding { Path = new PropertyPath("Background"), Source = userControl };
+            mwiChild.SetBinding(BackgroundProperty, b1);
+            var b2 = new Binding
+            {
+                Path = new PropertyPath("Background"),
+                Source = userControl,
+                Converter = ColorHslBrush.Instance,
+                ConverterParameter = "+75%"
+            };
+            mwiChild.SetBinding(ForegroundProperty, b2);
+
+            container.Children.Add(mwiChild);
+
+            await Task.Delay(1000);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            var a11 = GC.GetTotalMemory(true);
+
+            mwiChild.Close(null);
+
+            await Task.Delay(1000);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            var a12 = GC.GetTotalMemory(true);
+
+            Debug.Print($"Test{step}: {a11:N0}, {a12:N0}");
         }
     }
 }
