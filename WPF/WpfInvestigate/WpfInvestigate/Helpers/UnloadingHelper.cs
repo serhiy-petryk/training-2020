66c8991d72ae37fbd60f9f0cec8b3e3cffd3770e
serhiy-petryk <sergei_petrik@yahoo.com>
2021-05-12 02:14:53 +0300

    cleared UnloadingHelper

--- WPF/WpfInvestigate/WpfInvestigate/Helpers/UnloadingHelper.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Helpers/UnloadingHelper.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -69,36 +68,13 @@ namespace WpfInvestigate.Helpers
             // _itemCount += elements.Length;
             // Debug.Print($"Clean: {d.GetType().Name}, items: {_itemCount}, steps: {++_stepCount}");
 
-
-            // EventHelper.RemoveDPDEvents(elements);
-
             foreach (var element in elements)
             {
                 BindingOperations.ClearAllBindings(element);
                 EventHelper.RemoveWpfEventHandlers(element);
 
-                /*// foreach (var pi in GetPropertiesForCleaner(element.GetType()))
-                foreach (var pi in GetPropertiesForCleaner(element.GetType()).Where(a => a.Name != "TemplatedParent" && a.Name != "Parent" && a.Name != "Content" && a.Name != "DataContext"))
-                {
-                    var value = pi.GetValue(element);
-                    if (value is DependencyObject d1)
-                    {
-                        BindingOperations.ClearAllBindings(d1); // !! Important. Remove error on MwiStartup test (Layout transform)
-                        EventHelper.RemoveWpfEventHandlers(d1);
-                    }
-                    else if (value is ResourceDictionary rd)
-                        ClearResources(rd);
-                    //else if (value is ICollection c)
-                    {
-                        // EventHelper.RemoveWpfEventHandlers(c);
-                        //EventHelperOld.RemoveAllEventSubsriptions(c);
-                    }
-                }*/
-
                 ClearElement(element);
 
-                //if (element is UIElement uiElement && VisualTreeHelper.GetParent(uiElement) is DependencyObject o)
-                  //  RemoveChild(o, uiElement); // !!! Important
                 if (element is UIElement uiElement && VisualTreeHelper.GetParent(uiElement) is Panel panel && !panel.IsItemsHost)
                     panel.Children.Remove(uiElement);
 
@@ -108,143 +84,6 @@ namespace WpfInvestigate.Helpers
                     fe.Resources.Add("Unloaded", null);
                 }
             }
-
-            // EventHelper.RemoveDPDEvents(elements);
-
-            /*//var sw = new Stopwatch();
-            //sw.Start();
-
-            foreach (var element in elements)
-                BindingOperations.ClearAllBindings(element);
-            //var d1 = sw.ElapsedMilliseconds;
-            //sw.Restart();
-
-            foreach (var element in elements)
-                EventHelper.RemoveWpfEventHandlers(element);
-            //var d2 = sw.ElapsedMilliseconds;
-            //sw.Restart();
-
-            foreach (var fe in elements.OfType<FrameworkElement>())
-            {
-                ClearResources(fe.Resources);
-                // fe.Resources.Clear();
-            }
-            //var d3 = sw.ElapsedMilliseconds;
-            //sw.Restart();
-
-            foreach (var uiElement in elements.OfType<UIElement>())
-            {
-                if (VisualTreeHelper.GetParent(uiElement) is DependencyObject _do)
-                    RemoveChild(_do, uiElement); // !!! Important
-            }
-
-            foreach (var fe in elements.OfType<FrameworkElement>())
-                fe.Resources.Add("Unloaded", null);
-
-            //if (element is FrameworkElement fe)
-            //  fe.Resources.Add("Unloaded", null);
-
-            //var d4 = sw.ElapsedMilliseconds;
-            //sw.Stop();
-
-            // Debug.Print($"SW: {d1}, {d2}, {d3}, {d4}");*/
-
-            /*return;
-
-            foreach (var element in elements)
-            {
-                BindingOperations.ClearAllBindings(element);
-                EventHelper.RemoveWpfEventHandlers(element);
-
-                foreach (var pi in GetPropertiesForCleaner(element.GetType()))
-                {
-                    var value = pi.GetValue(element);
-                    if (value is DependencyObject d1)
-                    {
-                        // BindingOperations.ClearAllBindings(d1); // !! Important. Remove error on MwiStartup test (Layout transform)
-                        // EventHelper.RemoveWpfEventHandlers(d1);
-                    }
-                    else if (value is ResourceDictionary rd)
-                        ClearResources(rd);
-                    //else if (value is ICollection c)
-                    {
-                       // EventHelper.RemoveWpfEventHandlers(c);
-                        //EventHelperOld.RemoveAllEventSubsriptions(c);
-                    }
-                }
-
-                // ClearElement(element); // !! Very important
-
-                if (element is UIElement uIElement)
-                {
-                    //if (uIElement.CommandBindings.Count > 0)
-                      //  uIElement.CommandBindings.Clear();
-                    //if (uIElement.InputBindings.Count > 0)
-                      //  uIElement.InputBindings.Clear();
-                    //if (element is FrameworkElement fe2 && fe2.Triggers.Count > 0)
-                      //  fe2.Triggers.Clear();
-
-                    if (VisualTreeHelper.GetParent(uIElement) is DependencyObject _do)
-                        RemoveChild(_do, uIElement); // !!! Important
-                }
-
-                //if (element is FrameworkElement fe)
-                  //  fe.Resources.Add("Unloaded", null);
-            }*/
-        }
-
-        private static void RemoveChild(DependencyObject parent, UIElement child)
-        {
-            if (parent is Panel panel)
-            {
-                if (!panel.IsItemsHost)
-                    panel.Children.Remove(child);
-                return;
-            }
-            /* if (parent is Decorator decorator)
-            {
-                if (Equals(decorator.Child, child)) decorator.Child = null;
-                return;
-            }
-            if (parent is ContentPresenter contentPresenter)
-            {
-                if (Equals(contentPresenter.Content, child)) contentPresenter.Content = null;
-                return;
-            }
-            if (parent is ContentControl contentControl)
-            {
-                if (Equals(contentControl.Content, child)) contentControl.Content = null;
-                return;
-            }
-            if (parent is ContainerVisual container)
-            {
-                container.Children.Remove(child);
-                return;
-            }
-            if (parent is Menu menu)
-            {
-                if (menu.Items.Contains(child))
-                    menu.Items.Remove(child);
-                return;
-            }
-            if (parent is MenuItem menuItem)
-            {
-                if (menuItem.Items.Contains(child))
-                    menuItem.Items.Remove(child);
-                return;
-            }
-            if (parent is Slider || parent is Separator || parent is TabControl || parent is TextBox || parent is ItemsPresenter)
-            {
-                return;
-            }
-            if (parent.GetType().Namespace == "System.Windows.Controls.Primitives")
-            {
-                return;
-            }
-
-            Debug.Print($"RemoveChild not defined for : {parent.GetType()} type of parent");*/
-            return;
-            throw new Exception($"RemoveChildis not defined for {parent.GetType().Name} type of parent");
         }
 
         private static IEnumerable<DependencyObject> GetVChildren(DependencyObject current)
@@ -258,14 +97,10 @@ namespace WpfInvestigate.Helpers
                 yield return child;
             }
         }
-        #endregion
 
         private static void ClearElement(DependencyObject element)
         {
             if (element.IsSealed) return;
-
-            //if (element is Panel panel && !panel.IsItemsHost && panel.Children.Count != 0)
-               // panel.Children.Clear();
 
             foreach (var pi in element.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy)
                 .Where(pi => pi.CanWrite && !pi.PropertyType.IsValueType && pi.PropertyType != typeof(string)))
@@ -275,6 +110,7 @@ namespace WpfInvestigate.Helpers
                     pi.SetValue(element, null);
             }
         }
+        #endregion
 
     }
 }
