f5163e0b51277c8f03b8559eb2127fa9ca37fdba
serhiy-petryk <sergei_petrik@yahoo.com>
2021-05-18 21:08:24 +0300

    refactored DialogMessage.DataContext property

--- WPF/WpfInvestigate/WpfInvestigate/Controls/DialogMessage.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/DialogMessage.cs
@@ -112,7 +112,6 @@ namespace WpfInvestigate.Controls
 
         private DialogMessage()
         {
-            // DataContext = this;
             _cmdClickButton = new RelayCommand(OnButtonClick);
         }
 
