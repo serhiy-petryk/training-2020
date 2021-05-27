5eaa9436c72639617d2d6ee31d80400332f03876
serhiy-petryk <sergei_petrik@yahoo.com>
2021-05-28 00:08:15 +0300

    x

--- WPF/WpfInvestigate/WpfInvestigate/Controls/MwiContainer.cs
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/MwiContainer.cs
@@ -71,7 +71,7 @@ namespace WpfInvestigate.Controls
                                 mwiChild.Position = GetStartPositionForMwiChild(mwiChild);
                             MwiPanel?.Children.Add(mwiChild);
                             mwiChild.Activate();
-                        }), DispatcherPriority.Render);
+                        }), DispatcherPriority.Background);
                     }
                     break;
 
