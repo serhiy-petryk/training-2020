73b59a68284019a7af0435f15704f5b5ab38da3a
serhiy-petryk <sergei_petrik@yahoo.com>
2021-06-09 01:45:29 +0300

    converted WpfSpLib to DLL

--- WPF/WpfInvestigate/WpfSpLibDemo/Samples/ResizableInheritanceSample2.xaml.cs
+++ WPF/WpfInvestigate/WpfSpLibDemo/Samples/ResizableInheritanceSample2.xaml.cs
@@ -20,13 +20,16 @@ namespace WpfSpLibDemo.Samples
 
         private void ResizableInheritanceSample2_Loaded(object sender, RoutedEventArgs e)
         {
-            Loaded -= ResizableInheritanceSample2_Loaded;
+            // Error for DLL, because line
+            // 'DefaultStyleKeyProperty.OverrideMetadata(typeof(ResizingControl), new FrameworkPropertyMetadata(typeof(ResizingControl)))'
+            // in ResizingControl.cs not commented 
+            /* Loaded -= ResizableInheritanceSample2_Loaded;
             //Dispatcher.Invoke(() =>
             //{
                 var cp = Tips.GetVisualChildren(this).OfType<ContentPresenter>().FirstOrDefault();
                 var a2 = cp.ContentTemplate.FindName("MovingThumb", cp) as Thumb;
                 MovingThumb = a2;
-            //}, DispatcherPriority.DataBind);
+            //}, DispatcherPriority.DataBind);*/
         }
 
         // ==============================
