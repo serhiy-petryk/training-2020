7e6b64582e0f0dab79146fc0afbf227712bcd1ba
serhiy-petryk <sergei_petrik@yahoo.com>
2022-01-18 03:39:09 +0200

    x

---
+++ WPF/WpfInvestigate/WpfInvestigate/Controls/DragAdornerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfInvestigate.Controls
+{
+    public class DragAdornerControl: ItemsControl // , INotifyPropertyChanged
+    {
+        public static readonly DependencyProperty DragDropEffectProperty = DependencyProperty.Register("DragDropEffect",
+            typeof(DragDropEffects), typeof(DragAdornerControl), new FrameworkPropertyMetadata(DragDropEffects.Move));
+        public DragDropEffects DragDropEffect
+        {
+            get => (DragDropEffects)GetValue(DragDropEffectProperty);
+            set => SetValue(DragDropEffectProperty, value);
+        }
+
+        /*public DragDropEffects DragDropEffects { get; set; }= DragDropEffects.Move;
+
+        #region ===========  INotifyPropertyChanged  ===============
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertiesChanged(params string[] propertyNames)
+        {
+            foreach (var propertyName in propertyNames)
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion*/
+    }
+}
