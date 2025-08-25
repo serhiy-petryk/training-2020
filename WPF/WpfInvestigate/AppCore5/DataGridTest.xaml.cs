19def13f0a6881032fe732852e06a00d0951f807
serhiy-petryk <sergei_petrik@yahoo.com>
2025-08-25 15:44:49 +0300

    added DataGridTest in AppCore5

---
+++ WPF/WpfInvestigate/AppCore5/DataGridTest.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using AppCore5.Models;
+
+namespace AppCore5
+{
+    /// <summary>
+    /// Interaction logic for DataGridTest.xaml
+    /// </summary>
+    public partial class DataGridTest : Window
+    {
+        public DataGridTest()
+        {
+            InitializeComponent();
+
+            var users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgSimple.ItemsSource = users;
+        }
+    }
+}
---
+++ WPF/WpfInvestigate/AppCore5/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppCore5.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
